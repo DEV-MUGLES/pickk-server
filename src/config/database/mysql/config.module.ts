7f94842c2444dd3541665f94685873be926bd386
greatSumini <greatsumini@gmail.com>
2021-02-20 01:50:03 +0900

    feat(config): :wrench: add MysqlConfig

---
+++ src/config/database/mysql/config.module.ts
@@ -0,0 +1,29 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+import configuration from './configuration';
+import { MysqlConfigService } from './config.service';
+
+/**
+ * Import and provide postgres configuration related classes.
+ *
+ * @module
+ */
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+      validationSchema: Joi.object({
+        MYSQL_HOST: Joi.string().default('mysql'),
+        MYSQL_PORT: Joi.number().default(3306),
+        MYSQL_USERNAME: Joi.string().default('pickkdev'),
+        MYSQL_PASSWORD: Joi.string().default('pickkdev123'),
+        MYSQL_DATABASE: Joi.string().default('pickk_dev'),
+      }),
+    }),
+  ],
+  providers: [ConfigService, MysqlConfigService],
+  exports: [ConfigService, MysqlConfigService],
+})
+export class MysqlConfigModule {}
---
+++ src/config/database/mysql/config.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+/**
+ * Service dealing with mysql config based operations.
+ *
+ * @class
+ */
+@Injectable()
+export class MysqlConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get host(): string {
+    return this.configService.get<string>('mysql.host');
+  }
+  get port(): string {
+    return this.configService.get<string>('mysql.port');
+  }
+  get username(): string {
+    return this.configService.get<string>('mysql.username');
+  }
+  get password(): string {
+    return this.configService.get<string>('mysql.password');
+  }
+  get database(): string {
+    return this.configService.get<string>('mysql.database');
+  }
+}
