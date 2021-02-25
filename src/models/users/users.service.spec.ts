f42e27225b38903c47f6cc34155d0854deb9c8b9
greatSumini <greatsumini@gmail.com>
2021-02-25 22:44:50 +0900

    test(api): :white_check_mark: add users.service.spec.ts

---
+++ src/models/users/users.service.spec.ts
@@ -0,0 +1,58 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import * as faker from 'faker';
+import { CreateUserDto } from './dto';
+import { User } from './entities/user.entity';
+
+import { UsersRepository } from './users.repository';
+import { UsersService } from './users.service';
+
+describe('UsersService', () => {
+  let usersService: UsersService;
+  let usersRepository: UsersRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UsersService, UsersRepository],
+    }).compile();
+
+    usersService = module.get<UsersService>(UsersService);
+    usersRepository = module.get<UsersRepository>(UsersRepository);
+  });
+
+  describe('creating a user', () => {
+    it('create success', async () => {
+      const createUserDto: CreateUserDto = {
+        name: faker.lorem.sentence(),
+        password: faker.lorem.sentence(),
+        email: faker.internet.email(),
+      };
+
+      const newUser = Object.assign(new User(), createUserDto);
+
+      const userRepositoryCreateSpy = jest
+        .spyOn(usersRepository, 'createEntity')
+        .mockResolvedValue(newUser);
+
+      const result = await usersService.create(createUserDto);
+
+      expect(userRepositoryCreateSpy).toHaveBeenCalledWith(createUserDto);
+      expect(result).toEqual(newUser);
+    });
+  });
+
+  describe('get a user', () => {
+    it('get success', async () => {
+      const userId = faker.random.number();
+      const user = Object.assign(new User(), { id: userId });
+
+      const userRepositoryGetSpy = jest
+        .spyOn(usersRepository, 'get')
+        .mockResolvedValue(user);
+
+      const result = await usersService.get(userId);
+
+      expect(userRepositoryGetSpy).toHaveBeenCalledWith(userId, []);
+      expect(result).toEqual(user);
+    });
+  });
+});
