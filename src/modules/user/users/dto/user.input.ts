42ee2cd86fa991493c12582f57c5dfb2c0d0ce2e
greatSumini <greatsumini@gmail.com>
2021-03-04 11:17:05 +0900

    fix: :bug: fix CreateUserInput field error
    - ObjectType class를 상속해 InputType class를 생성하는 경우, MappedType의 third parameter로 변환될 decorator를 추가해줘야합니다.
    - ref: https://docs.nestjs.com/graphql/mapped-types

--- src/modules/user/users/dto/user.input.ts
+++ src/modules/user/users/dto/user.input.ts
@@ -3,15 +3,13 @@ import { InputType, PartialType, PickType, OmitType } from '@nestjs/graphql';
 import { User } from '../models/user.model';
 
 @InputType()
-export class CreateUserInput extends PickType(User, [
-  'email',
-  'name',
-  'password',
-  'weight',
-  'height',
-]) {}
+export class CreateUserInput extends PickType(
+  User,
+  ['email', 'name', 'password', 'weight', 'height'],
+  InputType
+) {}
 
 @InputType()
 export class UpdateUserInput extends PartialType(
-  OmitType(User, ['createdAt', 'updatedAt'])
+  OmitType(User, ['createdAt', 'updatedAt'], InputType)
 ) {}
