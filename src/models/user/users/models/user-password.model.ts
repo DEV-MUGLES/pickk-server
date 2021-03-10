ea308567f6913c44b4483acbbe9ce962561be360
greatSumini <greatsumini@gmail.com>
2021-03-11 00:46:56 +0900

    feat(model): :sparkles: add UserPassword model

--- src/models/user/users/models/user-password.model.ts
+++ src/models/user/users/models/user-password.model.ts
@@ -1,5 +1,19 @@
+import bcrypt from 'bcrypt';
+
 import { ObjectType } from '@nestjs/graphql';
 import { UserPasswordEntity } from '../entities/user-password.entity';
 
 @ObjectType()
-export class UserPassword extends UserPasswordEntity {}
+export class UserPassword extends UserPasswordEntity {
+  constructor(input: string) {
+    super();
+
+    this.salt = bcrypt.genSaltSync();
+    this.encrypted = bcrypt.hashSync(input, this.salt);
+    this.createdAt = new Date();
+  }
+
+  public compare(password: string): boolean {
+    return bcrypt.compareSync(password, this.encrypted);
+  }
+}
