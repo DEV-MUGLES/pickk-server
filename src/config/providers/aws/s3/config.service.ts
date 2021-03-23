13a060f5a428a4a9c13547d780c7574d04feb448
greatSumini <greatsumini@gmail.com>
2021-03-24 00:57:29 +0900

    feat(config): :wrench: add AwsS3Config (module, service, configuration)

---
+++ src/config/providers/aws/s3/config.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+/**
+ * Service dealing with mysql config based operations.
+ *
+ * @class
+ */
+@Injectable()
+export class AwsS3ConfigService {
+  constructor(private configService: ConfigService) {}
+
+  get region(): string {
+    return this.configService.get<string>('aws-s3.region');
+  }
+  get accessKeyId(): string {
+    return this.configService.get<string>('aws-s3.accessKeyId');
+  }
+  get secretAccessKey(): string {
+    return this.configService.get<string>('aws-s3.secretAccessKey');
+  }
+  get publicBucketName(): string {
+    return this.configService.get<string>('aws-s3.publicBucketName');
+  }
+}
