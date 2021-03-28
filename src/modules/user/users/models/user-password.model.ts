0f42032090cc2fda1558831615cff8b307ed6276
greatSumini <greatsumini@gmail.com>
2021-03-29 00:39:05 +0900

    refactor(domain): :recycle: fix UserPassword access modifiers

--- src/modules/user/users/models/user-password.model.ts
+++ src/modules/user/users/models/user-password.model.ts
@@ -6,8 +6,7 @@ import { UserPasswordInvalidException } from '../exceptions/user.exception';
 
 @ObjectType()
 export class UserPassword extends UserPasswordEntity {
-  public static minLength = 8;
-  public static validRegex = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_=(),./:;{}[\]|\\<>-]).{8,}$/;
+  private static validRegex = /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*_=(),./:;{}[\]|\\<>-]).{8,}$/;
 
   public static create(password: string): UserPassword {
     if (!this.validate(password)) {
@@ -27,12 +26,12 @@ export class UserPassword extends UserPasswordEntity {
    * @param password 검증할 비밀번호 문자열
    * @returns 통과 여부 boolean
    */
-  public static validate(password: string): boolean {
+  private static validate(password: string): boolean {
     if (typeof password !== 'string') {
       return false;
     }
 
-    return password.match(this.validRegex) !== null;
+    return this.validRegex.test(password);
   }
 
   constructor(attributes?: UserPasswordEntity) {
