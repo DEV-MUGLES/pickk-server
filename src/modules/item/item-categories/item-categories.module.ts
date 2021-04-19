ccbb4d543632343b69f28cafac5e7693d15ff486
greatSumini <greatsumini@gmail.com>
2021-04-19 18:42:03 +0900

    feat(infra): add ItemCategoriesRepository

--- src/modules/item/item-categories/item-categories.module.ts
+++ src/modules/item/item-categories/item-categories.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
-@Module({})
+import { ItemCategoriesRepository } from './item-categories.repository';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([ItemCategoriesRepository])],
+})
 export class ItemCategoriesModule {}
