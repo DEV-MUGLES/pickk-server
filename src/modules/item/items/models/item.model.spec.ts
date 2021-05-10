b3427f5a26550794a2b0818fdb228996758e2e70
greatSumini <greatsumini@gmail.com>
2021-05-10 18:28:15 +0900

    feat(application): :sparkles: add ItemResolver addItemPrice, removeItemPrice

--- src/modules/item/items/models/item.model.spec.ts
+++ src/modules/item/items/models/item.model.spec.ts
@@ -1,6 +1,9 @@
 import * as faker from 'faker';
 
+import { AddItemPriceInput } from '../dtos/item-price.input';
 import { AddItemUrlInput } from '../dtos/item-url.input';
+import { ItemPrice } from './item-price.model';
+
 import { Item } from './item.model';
 
 describe('Item', () => {
@@ -10,13 +13,13 @@ describe('Item', () => {
       isPrimary: true,
     };
 
-    it('shoud return added url', () => {
+    it('추가된 URL을 반환한다.', () => {
       const item = new Item();
       const result = item.addUrl(addItemUrlInput);
       expect(result).toMatchObject(addItemUrlInput);
     });
 
-    it('shoud first url be primary', () => {
+    it('첫번째 URL은 Primary로 설정된다.', () => {
       addItemUrlInput.isPrimary = false;
       const item = new Item();
       const result = item.addUrl(addItemUrlInput);
@@ -24,6 +27,73 @@ describe('Item', () => {
         ...addItemUrlInput,
         isPrimary: true,
       });
+    });
+  });
+
+  describe('addPrice', () => {
+    const [pickkDiscountAmount, sellPrice, originalPrice] = [
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+    ].sort();
+    const finalPrice = sellPrice - pickkDiscountAmount;
+
+    const addItemPriceInput: AddItemPriceInput = {
+      originalPrice,
+      sellPrice,
+      pickkDiscountAmount,
+      finalPrice,
+      isActive: true,
+      isCrawlUpdating: true,
+    };
+
+    it('추가된 Price를 반환한다.', () => {
+      const item = new Item();
+      const result = item.addPrice(addItemPriceInput);
+      expect(result).toMatchObject(addItemPriceInput);
+    });
+
+    it('isActive가 true면 기존 Price들을 비활성화한다.', () => {
+      const item = new Item({
+        prices: [
+          new ItemPrice(addItemPriceInput),
+          new ItemPrice(addItemPriceInput),
+        ],
+      });
+      item.addPrice(addItemPriceInput);
+      expect(item.prices[0].isActive).toEqual(false);
+      expect(item.prices[1].isActive).toEqual(false);
+    });
+  });
+
+  describe('removePrice', () => {
+    const [pickkDiscountAmount, sellPrice, originalPrice] = [
+      faker.datatype.number(),
+      faker.datatype.number(),
+      faker.datatype.number(),
+    ].sort();
+    const finalPrice = sellPrice - pickkDiscountAmount;
+
+    const addItemPriceInput: AddItemPriceInput = {
+      originalPrice,
+      sellPrice,
+      pickkDiscountAmount,
+      finalPrice,
+      isActive: true,
+      isCrawlUpdating: true,
+    };
+
+    it('성공적으로 삭제한다.', () => {
+      const priceId = faker.datatype.number();
+      const item = new Item({
+        prices: [
+          new ItemPrice(),
+          new ItemPrice({ ...addItemPriceInput, id: priceId }),
+        ],
+      });
+      const result = item.removePrice(priceId);
+      expect(result.id).toEqual(priceId);
+      expect(item.prices.length).toEqual(1);
     });
   });
 });
