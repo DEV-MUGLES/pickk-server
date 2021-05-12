ce314002e6b0e0c531108ebe661bbec7d6ebbfa6
greatSumini <greatsumini@gmail.com>
2021-05-12 12:43:41 +0900

    enhance(application): :art: change randomNickname logic
    - remove number postfix, add space between adj - noun

--- src/authentication/helpers/auth.helper.ts
+++ src/authentication/helpers/auth.helper.ts
@@ -4,12 +4,9 @@ import {
 } from '@src/authentication/constants/nickname.constant';
 
 export const genRandomNickname = () => {
-  let number = Math.abs(Math.random() * 100 - 1).toFixed(0);
-  if (Number(number) < 10) number = '0' + number;
-
   return (
     randomNicknameAdj[Math.floor(Math.random() * randomNicknameAdj.length)] +
-    randomNicknameNoun[Math.floor(Math.random() * randomNicknameNoun.length)] +
-    number
+    ' ' +
+    randomNicknameNoun[Math.floor(Math.random() * randomNicknameNoun.length)]
   );
 };
