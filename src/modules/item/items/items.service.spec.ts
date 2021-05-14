6798b149d0ef91212abfc062dec0a39ab5243d63
greatSumini <greatsumini@gmail.com>
2021-05-14 17:15:54 +0900

    test: :white_check_mark: add ItemsService updateByCrawlData tests

--- src/modules/item/items/items.service.spec.ts
+++ src/modules/item/items/items.service.spec.ts
@@ -1,9 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ISpiderItem } from '@src/providers/spider/interfaces/spider.interface';
 import * as faker from 'faker';
 
 import { AddItemUrlInput } from './dtos/item-url.input';
 import { ItemsRepository } from './items.repository';
 import { ItemsService } from './items.service';
+import { ItemPrice } from './models/item-price.model';
 import { ItemUrl } from './models/item-url.model';
 import { Item } from './models/item.model';
 
@@ -63,6 +65,57 @@ describe('ItemsService', () => {
       expect(result).toEqual(url);
       expect(itemAddUrlSpy).toHaveBeenCalledWith(addItemUrlInput);
       expect(repositorySaveSpy).toHaveBeenCalledTimes(1);
+    });
+  });
+
+  describe('updateByCrawlData', () => {
+    const spiderItem: ISpiderItem = {
+      name: faker.lorem.text(),
+      brandKor: faker.lorem.text(),
+      imageUrl: faker.image.imageUrl(),
+      originalPrice: faker.datatype.number(),
+      salePrice: faker.datatype.number(),
+      url: faker.internet.url(),
+    };
+    it('success', async () => {
+      const item = new Item({
+        prices: [
+          new ItemPrice({
+            isCrawlUpdating: true,
+            pickkDiscountRate: 5,
+          }),
+        ],
+      });
+      const itemsRepositorySaveSpy = jest
+        .spyOn(itemsRepository, 'save')
+        .mockImplementationOnce(async (v) => v as any);
+
+      const result = await itemsService.updateByCrawlData(item, spiderItem);
+      expect(result.name).toEqual(spiderItem.name);
+      expect(result.prices[0].originalPrice).toEqual(spiderItem.originalPrice);
+      expect(result.prices[0].sellPrice).toEqual(spiderItem.salePrice);
+      expect(itemsRepositorySaveSpy).toHaveBeenCalledTimes(1);
+    });
+
+    it('isCrawlUpdaing=false인 price는 업데이트하지 않는다.', async () => {
+      const item = new Item({
+        prices: [
+          new ItemPrice({
+            isCrawlUpdating: false,
+            pickkDiscountRate: 5,
+          }),
+        ],
+      });
+
+      const itemsRepositorySaveSpy = jest
+        .spyOn(itemsRepository, 'save')
+        .mockImplementationOnce(async (v) => v as any);
+
+      const result = await itemsService.updateByCrawlData(item, spiderItem);
+      expect(result.name).toEqual(spiderItem.name);
+      expect(result.prices[0].originalPrice).toBeFalsy();
+      expect(result.prices[0].sellPrice).toBeFalsy();
+      expect(itemsRepositorySaveSpy).toHaveBeenCalledTimes(1);
     });
   });
 });
