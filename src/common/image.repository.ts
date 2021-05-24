fa13a8dbed5658f51756d693e51e808276e2f232
greatSumini <greatsumini@gmail.com>
2021-05-24 14:31:51 +0900

    feat(common): :sparkles: add ImageRepository
    - for entities which extends AbstractImageEntity

---
+++ src/common/image.repository.ts
@@ -0,0 +1,72 @@
+import { Repository, DeepPartial, FindOneOptions } from 'typeorm';
+import { NotFoundException } from '@nestjs/common';
+
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
+import { MultipleEntityReturnedException } from '@src/common/exceptions/multiple-entity-returned.exception';
+import { AbstractImageEntity } from './entities/image.entity';
+
+export abstract class ImageRepository<
+  Entity extends AbstractImageEntity,
+  Model extends AbstractImageEntity
+> extends Repository<Entity> {
+  private isEntity(obj: unknown): obj is Entity {
+    return obj !== undefined && (obj as Entity).key !== undefined;
+  }
+
+  async get(key: string, relations: string[] = []): Promise<Model | null> {
+    return await this.findOne({
+      where: { key },
+      relations,
+    })
+      .then((entity) => {
+        if (!this.isEntity(entity)) {
+          throw new NotFoundException('Model not found.');
+        }
+
+        return Promise.resolve(this.entityToModel(entity));
+      })
+      .catch((error) => Promise.reject(error));
+  }
+
+  async createEntity(
+    inputs: DeepPartial<Entity>,
+    relations: string[] = []
+  ): Promise<Model | null> {
+    return this.save(inputs)
+      .then(async (entity) => await this.get((entity as Entity).key, relations))
+      .catch((error) => Promise.reject(error));
+  }
+
+  async updateEntity(
+    entity: Model,
+    inputs: QueryDeepPartialEntity<Entity>,
+    relations: string[] = []
+  ): Promise<Model | null> {
+    return this.update(entity.key, { ...inputs })
+      .then(async () => await this.get(entity.key, relations))
+      .catch((error) => Promise.reject(error));
+  }
+
+  async findOneEntity(
+    param: FindOneOptions<Model>['where'],
+    relations: string[] = []
+  ): Promise<Model | null> {
+    return await this.find({
+      where: param,
+      relations,
+    }).then((entities) => {
+      if (entities.length > 1) {
+        throw new MultipleEntityReturnedException();
+      }
+      if (entities.length === 0 || !this.isEntity(entities[0])) {
+        return null;
+      }
+
+      return Promise.resolve(this.entityToModel(entities[0]));
+    });
+  }
+
+  abstract entityToModel(entity: Entity, transformOptions?);
+
+  abstract entityToModelMany(entities: Entity[], transformOptions?);
+}
--- src/modules/common/images/base-image.repository.ts
+++ src/modules/common/images/base-image.repository.ts
@@ -1,39 +1,15 @@
-import { NotFoundException } from '@nestjs/common';
-import { getConnection, EntityRepository, Repository } from 'typeorm';
+import { getConnection, EntityRepository } from 'typeorm';
 import { plainToClass } from 'class-transformer';
 
 import { BaseImageEntity } from './entities/base-image.entity';
 import { BaseImage } from './models/base-image.model';
+import { ImageRepository } from '@src/common/image.repository';
 
 @EntityRepository(BaseImageEntity)
-export class BaseImageRepository extends Repository<BaseImageEntity> {
-  private isEntity(obj: unknown): obj is BaseImageEntity {
-    return obj !== undefined && (obj as BaseImageEntity).key !== undefined;
-  }
-
-  async getByKey(key: string): Promise<BaseImage | null> {
-    return await this.findOne({
-      where: { key },
-    })
-      .then((entity) => {
-        if (!this.isEntity(entity)) {
-          throw new NotFoundException('BaseImage not found.');
-        }
-
-        return Promise.resolve(this.entityToModel(entity));
-      })
-      .catch((error) => Promise.reject(error));
-  }
-
-  async bulkInsert(keys: string[]): Promise<void> {
-    await getConnection()
-      .createQueryBuilder()
-      .insert()
-      .into(BaseImageEntity)
-      .values(keys.map((key) => ({ key, rangle: 0 })))
-      .execute();
-  }
-
+export class BaseImageRepository extends ImageRepository<
+  BaseImageEntity,
+  BaseImage
+> {
   entityToModel(entity: BaseImageEntity, transformOptions = {}): BaseImage {
     return plainToClass(BaseImage, entity, transformOptions) as BaseImage;
   }
@@ -45,5 +21,14 @@ export class BaseImageRepository extends Repository<BaseImageEntity> {
     return entities.map((entity) =>
       this.entityToModel(entity, transformOptions)
     );
+  }
+
+  async bulkInsert(keys: string[]): Promise<void> {
+    await getConnection()
+      .createQueryBuilder()
+      .insert()
+      .into(BaseImageEntity)
+      .values(keys.map((key) => ({ key, rangle: 0 })))
+      .execute();
   }
 }
