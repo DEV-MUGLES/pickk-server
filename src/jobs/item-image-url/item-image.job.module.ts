7b3d3c760fd6b332685652a1499ccb5c59f2e2b5
greatSumini <greatsumini@gmail.com>
2021-05-25 18:06:46 +0900

    feat(job): :sparkles: add ItemImageUrlJobModule, ItemImageUrlProducer

---
+++ src/jobs/item-image-url/item-image.job.module.ts
@@ -0,0 +1,24 @@
+import { forwardRef, Module } from '@nestjs/common';
+import { BullModule } from '@nestjs/bull';
+
+import { ItemsModule } from '@src/modules/item/items/items.module';
+
+import { ItemImageUrlConsumer } from './item-image-url.consumer';
+import { ItemImageUrlProducer } from './item-image.producer';
+
+import { ITEM_IMAGE_URL_QUEUE_NAME } from './item-image-url.constant';
+
+@Module({
+  imports: [
+    BullModule.registerQueue({
+      name: ITEM_IMAGE_URL_QUEUE_NAME,
+      settings: {
+        maxStalledCount: 500,
+      },
+    }),
+    forwardRef(() => ItemsModule),
+  ],
+  providers: [ItemImageUrlProducer, ItemImageUrlConsumer],
+  exports: [ItemImageUrlProducer],
+})
+export class ItemImageUrlJobModule {}
