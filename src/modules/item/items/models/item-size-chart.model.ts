83d39e4fa72dea61f270822931642fd4fe6d5ebb
greatSumini <greatsumini@gmail.com>
2021-05-25 21:41:39 +0900

    refactor(domain): :fire: remove ItemSizeChart.update

--- src/modules/item/items/models/item-size-chart.model.ts
+++ src/modules/item/items/models/item-size-chart.model.ts
@@ -1,14 +1,6 @@
 import { ObjectType } from '@nestjs/graphql';
-import { UpdateItemSizeChartInput } from '../dtos/item-size-chart.input';
 
 import { ItemSizeChartEntity } from '../entities/item-size-chart.entity';
 
 @ObjectType()
-export class ItemSizeChart extends ItemSizeChartEntity {
-  public update = (updateItemSizeChartInput: UpdateItemSizeChartInput) => {
-    if (!updateItemSizeChartInput) return;
-    for (const [key, value] of Object.entries(updateItemSizeChartInput)) {
-      this[key] = value;
-    }
-  };
-}
+export class ItemSizeChart extends ItemSizeChartEntity {}
