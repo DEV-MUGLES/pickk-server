4c811904476dbc3ef8342d7d6c98a9beb582f434
Yang <ybh1760@gmail.com>
2021-06-03 16:25:10 +0900

    fix(infra): :bug: remove itemCategory name unique index

--- src/database/migrations/1622702713491-IncreaseItemCategoryCodeNameSize.ts
+++ src/database/migrations/1622702713491-IncreaseItemCategoryCodeNameSize.ts
@@ -15,6 +15,9 @@ export class IncreaseItemCategoryCodeNameSize1622702713491
       'ALTER TABLE `item_category` ADD UNIQUE INDEX `IDX_d07bbc72c8822787efab782a6c` (`code`)'
     );
     await queryRunner.query(
+      'DROP INDEX `IDX_3776df63b26aee9f2089a70403` ON `item_category`'
+    );
+    await queryRunner.query(
       'ALTER TABLE `item_category` MODIFY `name` varchar(20) NOT NULL'
     );
   }
@@ -22,6 +25,9 @@ export class IncreaseItemCategoryCodeNameSize1622702713491
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
       'ALTER TABLE `item_category` MODIFY `name` varchar(10) NOT NULL'
+    );
+    await queryRunner.query(
+      'CREATE UNIQUE INDEX `IDX_3776df63b26aee9f2089a70403` ON `item_category` (`name`)'
     );
     await queryRunner.query(
       'ALTER TABLE `item_category` DROP INDEX `IDX_d07bbc72c8822787efab782a6c`'
