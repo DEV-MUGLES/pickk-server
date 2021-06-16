40298ac28055451ed087be606f40a497208505a4
greatSumini <greatSumini@gmail.com>
2021-06-16 14:37:48 +0900

    perf(application): :sparkles: add caching to PointsService.getAvailableAmountByUserId

--- src/modules/order/points/models/point-event.model.ts
+++ src/modules/order/points/models/point-event.model.ts
@@ -3,4 +3,8 @@ import { ObjectType } from '@nestjs/graphql';
 import { PointEventEntity } from '../entities/point-event.entity';
 
 @ObjectType()
-export class PointEvent extends PointEventEntity {}
+export class PointEvent extends PointEventEntity {
+  public static getAmountCacheKey(userId: number): string {
+    return `peA:${userId}`;
+  }
+}
