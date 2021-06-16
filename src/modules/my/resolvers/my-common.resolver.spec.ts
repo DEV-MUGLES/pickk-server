9c95e3a68773f3908b7f51a49c155f99e338df69
greatSumini <greatSumini@gmail.com>
2021-06-16 21:56:42 +0900

    fix: :white_check_mark: fix jest errors

--- src/modules/my/resolvers/my-common.resolver.spec.ts
+++ src/modules/my/resolvers/my-common.resolver.spec.ts
@@ -1,13 +1,10 @@
 import { JwtService } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as faker from 'faker';
-import { FileUpload } from 'graphql-upload';
 
 import { JwtPayload } from '@auth/dto/jwt.dto';
-import { AwsS3ProviderModule, AwsS3ProviderService } from '@providers/aws/s3';
+import { AwsS3ProviderModule } from '@providers/aws/s3';
 
-import { UpdateUserInput } from '@user/users/dtos/user.input';
-import { User, UserAvatarImage } from '@user/users/models';
 import { UsersRepository } from '@user/users/users.repository';
 import { UsersService } from '@user/users/users.service';
 
@@ -17,8 +14,6 @@ const JWT_TOKEN = 'JWT_TOKEN';
 
 describe('MyCommonResolver', () => {
   let myCommonResolver: MyCommonResolver;
-  let usersService: UsersService;
-  let awsS3ProviderService: AwsS3ProviderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -37,10 +32,6 @@ describe('MyCommonResolver', () => {
     }).compile();
 
     myCommonResolver = module.get<MyCommonResolver>(MyCommonResolver);
-    usersService = module.get<UsersService>(UsersService);
-    awsS3ProviderService = module.get<AwsS3ProviderService>(
-      AwsS3ProviderService
-    );
   });
 
   it('should be defined', () => {
@@ -53,112 +44,11 @@ describe('MyCommonResolver', () => {
     iat: new Date().getTime(),
     exp: new Date().getTime(),
   };
-  const user = new User({
-    id: payload.sub,
-    nickname: payload.nickname,
-  });
 
   describe('myJwtPayload', () => {
     it('should return payload', () => {
       const result = myCommonResolver.myJwtPayload(payload);
       expect(result).toEqual(payload);
-    });
-  });
-
-  describe('me', () => {
-    it('should return current user', async () => {
-      const usersServiceGetSpy = jest
-        .spyOn(usersService, 'get')
-        .mockResolvedValueOnce(user);
-
-      const result = await myCommonResolver.me(payload);
-
-      expect(result).toEqual(user);
-      expect(usersServiceGetSpy).toHaveBeenCalledWith(payload.sub, []);
-    });
-  });
-
-  describe('updateMe', () => {
-    it('should return updated user', async () => {
-      const updateUserInput: UpdateUserInput = {
-        name: faker.lorem.text(),
-      };
-      const usersServiceUpdateSpy = jest
-        .spyOn(usersService, 'update')
-        .mockResolvedValueOnce(new User({ ...user, ...updateUserInput }));
-
-      const result = await myCommonResolver.updateMe(payload, updateUserInput);
-
-      expect(result.name).toEqual(updateUserInput.name);
-      expect(usersServiceUpdateSpy).toHaveBeenCalledWith(
-        payload.sub,
-        updateUserInput
-      );
-    });
-  });
-
-  describe('updateMyAvatarImage', () => {
-    it('should return avatarImage when success', async () => {
-      const fileUpload: FileUpload = {
-        filename: faker.system.fileName(),
-        mimetype: faker.system.mimeType(),
-        createReadStream: () => null,
-        encoding: faker.lorem.text(),
-      };
-      const file = new Promise<FileUpload>((resolve) => resolve(fileUpload));
-
-      const s3UploadResult = {
-        url: faker.internet.url(),
-        key: faker.lorem.text(50),
-      };
-
-      const user = new User();
-
-      const awsS3ServiceUploadStreamSpy = jest
-        .spyOn(awsS3ProviderService, 'uploadStream')
-        .mockResolvedValueOnce(s3UploadResult);
-      const usersServiceUpdateAvatarImageSpy = jest
-        .spyOn(usersService, 'updateAvatarImage')
-        .mockResolvedValueOnce(
-          new UserAvatarImage({ key: s3UploadResult.key })
-        );
-
-      const result = await myCommonResolver.updateMyAvatarImage(user, { file });
-      expect(result.key).toEqual(s3UploadResult.key);
-      expect(awsS3ServiceUploadStreamSpy).toHaveBeenCalledWith(
-        fileUpload.createReadStream(),
-        fileUpload.filename,
-        fileUpload.mimetype
-      );
-      expect(usersServiceUpdateAvatarImageSpy).toHaveBeenCalledWith(
-        user,
-        s3UploadResult.key
-      );
-    });
-  });
-
-  describe('updateMyPassword', () => {
-    it('should return undefined when success', async () => {
-      const user = new User();
-      const oldPassword = faker.lorem.text(),
-        newPassword = faker.lorem.text();
-
-      const usersServiceUpdatePasswordSpy = jest
-        .spyOn(usersService, 'updatePassword')
-        .mockResolvedValueOnce(undefined);
-
-      const result = await myCommonResolver.updateMyPassword(
-        user,
-        oldPassword,
-        newPassword
-      );
-
-      expect(result).toEqual(undefined);
-      expect(usersServiceUpdatePasswordSpy).toHaveBeenCalledWith(
-        user,
-        oldPassword,
-        newPassword
-      );
     });
   });
 });
--- src/modules/user/users/users.resolver.spec.ts
+++ src/modules/user/users/users.resolver.spec.ts
@@ -1,39 +1,154 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import * as faker from 'faker';
+import { FileUpload } from 'graphql-upload';
+
+import { JwtPayload } from '@auth/dto/jwt.dto';
+import { AwsS3ProviderModule, AwsS3ProviderService } from '@providers/aws/s3';
+
 import { User } from './models/user.model';
-import { UsersRepository } from './users.repository';
 import { UsersResolver } from './users.resolver';
 import { UsersService } from './users.service';
+import { UserAvatarImage } from './models';
+import { UpdateUserInput } from './dtos/user.input';
+import { PointsService } from '@order/points/points.service';
 
 describe('UsersResolver', () => {
   let usersResolver: UsersResolver;
   let usersService: UsersService;
+  let awsS3ProviderService: AwsS3ProviderService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersResolver, UsersService, UsersRepository],
+      imports: [AwsS3ProviderModule],
+      providers: [
+        UsersResolver,
+        {
+          provide: UsersService,
+          useValue: new UsersService(null),
+        },
+        {
+          provide: PointsService,
+          useValue: new PointsService(null, null, null),
+        },
+      ],
     }).compile();
 
     usersResolver = module.get<UsersResolver>(UsersResolver);
     usersService = module.get<UsersService>(UsersService);
+    awsS3ProviderService = module.get<AwsS3ProviderService>(
+      AwsS3ProviderService
+    );
   });
 
   it('should be defined', () => {
     expect(usersResolver).toBeDefined();
   });
 
-  describe('user', () => {
-    it('should return matched user', async () => {
-      const userId = faker.datatype.number();
-      const user = new User({ id: userId });
+  const payload: JwtPayload = {
+    nickname: faker.lorem.text(),
+    sub: faker.datatype.number(),
+    iat: new Date().getTime(),
+    exp: new Date().getTime(),
+  };
+  const user = new User({
+    id: payload.sub,
+    nickname: payload.nickname,
+  });
 
-      const usersServiceSpy = jest
+  describe('me', () => {
+    it('should return current user', async () => {
+      const usersServiceGetSpy = jest
         .spyOn(usersService, 'get')
-        .mockResolvedValue(user);
+        .mockResolvedValueOnce(user);
 
-      const result = await usersResolver.user(userId);
+      const result = await usersResolver.me(payload);
+
       expect(result).toEqual(user);
-      expect(usersServiceSpy).toHaveBeenCalledWith(userId, []);
+      expect(usersServiceGetSpy).toHaveBeenCalledWith(payload.sub, []);
+    });
+  });
+
+  describe('updateMe', () => {
+    it('should return updated user', async () => {
+      const updateUserInput: UpdateUserInput = {
+        name: faker.lorem.text(),
+      };
+      const usersServiceUpdateSpy = jest
+        .spyOn(usersService, 'update')
+        .mockResolvedValueOnce(new User({ ...user, ...updateUserInput }));
+
+      const result = await usersResolver.updateMe(payload, updateUserInput);
+
+      expect(result.name).toEqual(updateUserInput.name);
+      expect(usersServiceUpdateSpy).toHaveBeenCalledWith(
+        payload.sub,
+        updateUserInput
+      );
+    });
+  });
+
+  describe('updateMyAvatarImage', () => {
+    it('should return avatarImage when success', async () => {
+      const fileUpload: FileUpload = {
+        filename: faker.system.fileName(),
+        mimetype: faker.system.mimeType(),
+        createReadStream: () => null,
+        encoding: faker.lorem.text(),
+      };
+      const file = new Promise<FileUpload>((resolve) => resolve(fileUpload));
+
+      const s3UploadResult = {
+        url: faker.internet.url(),
+        key: faker.lorem.text(50),
+      };
+
+      const user = new User();
+
+      const awsS3ServiceUploadStreamSpy = jest
+        .spyOn(awsS3ProviderService, 'uploadStream')
+        .mockResolvedValueOnce(s3UploadResult);
+      const usersServiceUpdateAvatarImageSpy = jest
+        .spyOn(usersService, 'updateAvatarImage')
+        .mockResolvedValueOnce(
+          new UserAvatarImage({ key: s3UploadResult.key })
+        );
+
+      const result = await usersResolver.updateMyAvatarImage(user, { file });
+      expect(result.key).toEqual(s3UploadResult.key);
+      expect(awsS3ServiceUploadStreamSpy).toHaveBeenCalledWith(
+        fileUpload.createReadStream(),
+        fileUpload.filename,
+        fileUpload.mimetype
+      );
+      expect(usersServiceUpdateAvatarImageSpy).toHaveBeenCalledWith(
+        user,
+        s3UploadResult.key
+      );
+    });
+  });
+
+  describe('updateMyPassword', () => {
+    it('should return undefined when success', async () => {
+      const user = new User();
+      const oldPassword = faker.lorem.text(),
+        newPassword = faker.lorem.text();
+
+      const usersServiceUpdatePasswordSpy = jest
+        .spyOn(usersService, 'updatePassword')
+        .mockResolvedValueOnce(undefined);
+
+      const result = await usersResolver.updateMyPassword(
+        user,
+        oldPassword,
+        newPassword
+      );
+
+      expect(result).toEqual(undefined);
+      expect(usersServiceUpdatePasswordSpy).toHaveBeenCalledWith(
+        user,
+        oldPassword,
+        newPassword
+      );
     });
   });
 });
