91d5cbfaea3588b5920fe7085ee9391594a7bad1
greatSumini <greatSumini@gmail.com>
2021-06-16 14:37:48 +0900

    perf(application): :sparkles: add caching to PointsService.getAvailableAmountByUserId

--- src/modules/order/points/points.service.ts
+++ src/modules/order/points/points.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { plainToClass } from 'class-transformer';
 
 import { PageInput } from '@common/dtos/pagination.dto';
 import { parseFilter } from '@common/helpers/filter.helpers';
+import { CacheService } from '@providers/cache/redis/provider.service';
 
 import { PointEventFilter } from './dtos/point-event.filter';
 import { PointEvent } from './models/point-event.model';
@@ -13,11 +14,24 @@ import { PointEventsRepository } from './points.repository';
 export class PointsService {
   constructor(
     @InjectRepository(PointEventsRepository)
-    private readonly pointEventsRepository: PointEventsRepository
+    private readonly pointEventsRepository: PointEventsRepository,
+    @Inject(CacheService) private cacheService: CacheService
   ) {}
 
-  async getAvailableAmountByUserId(userId: number) {
-    return await this.pointEventsRepository.getSum(userId);
+  async getAvailableAmountByUserId(userId: number, isUpdatingCache = true) {
+    const cacheKey = PointEvent.getAmountCacheKey(userId);
+    const cachedCount = await this.cacheService.get<number>(cacheKey);
+
+    if (cachedCount != null) {
+      return cachedCount;
+    }
+
+    const amount = await this.pointEventsRepository.getSum(userId);
+
+    if (isUpdatingCache) {
+      this.cacheService.set<number>(cacheKey, amount);
+    }
+    return amount;
   }
 
   async list(
