3a9f1146e03ccae88fa9a2a1c28da313739b0dbf
Yang <ybh1760@gmail.com>
2021-06-22 15:54:14 +0900

    feat(application): :art: add type to PointEventFilter

--- src/modules/order/points/dtos/point-event.filter.ts
+++ src/modules/order/points/dtos/point-event.filter.ts
@@ -1,4 +1,6 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsEnum } from 'class-validator';
+import { PointType } from '../constants/points.enum';
 
 import { IPointEvent } from '../interfaces/point-event.interface';
 
@@ -9,6 +11,10 @@ export class PointEventFilter implements Partial<IPointEvent> {
 
   @Field({ nullable: true })
   createdAtLte: Date;
+
+  @Field(() => PointType, { nullable: true })
+  @IsEnum(PointType)
+  type: PointType;
 
   @Field(() => Int, {
     nullable: true,
