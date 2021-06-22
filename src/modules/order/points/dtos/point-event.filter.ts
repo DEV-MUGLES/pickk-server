b2252a1096d1d92ae95674f00a8bd89271145de1
양병훈 <ybh1760@gmail.com>
2021-06-22 16:12:08 +0900

    feat(application): :art: add type field to PointEventFilter (#59)
    * feat(application): :art: add type field to PointEventFilter
    
    * enhance(application): :art: add IsOptional to PointEventFilter type field

--- src/modules/order/points/dtos/point-event.filter.ts
+++ src/modules/order/points/dtos/point-event.filter.ts
@@ -1,14 +1,21 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
+import { IsEnum, IsOptional } from 'class-validator';
+import { PointType } from '../constants/points.enum';
 
 import { IPointEvent } from '../interfaces/point-event.interface';
 
 @InputType()
 export class PointEventFilter implements Partial<IPointEvent> {
   @Field({ nullable: true })
-  createdAtMte: Date;
+  createdAtMte?: Date;
 
   @Field({ nullable: true })
-  createdAtLte: Date;
+  createdAtLte?: Date;
+
+  @Field(() => PointType, { nullable: true })
+  @IsEnum(PointType)
+  @IsOptional()
+  type?: PointType;
 
   @Field(() => Int, {
     nullable: true,
