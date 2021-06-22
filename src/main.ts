ca158498ce1f72ad4a4293c18c1a7db2d45fbfc8
Yang <ybh1760@gmail.com>
2021-06-22 13:29:10 +0900

    feat(application): :sparkles: add swagger module

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { AppConfigService } from './config/app/config.service';
 
@@ -8,6 +9,15 @@ const APP_CONFIG_SERVICE = 'AppConfigService';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const appConfig: AppConfigService = app.get(APP_CONFIG_SERVICE);
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('pickk-server')
+    .setDescription('The pickk-server API description')
+    .setVersion('1.0')
+    .addTag('spider')
+    .build();
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+
+  SwaggerModule.setup('api', app, swaggerDocument);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
 
