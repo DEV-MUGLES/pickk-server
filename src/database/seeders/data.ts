a0fb7bf9fcc29bc7f69b4d14d1dcbd13ef315276
Yang <ybh1760@gmail.com>
2021-06-25 17:27:39 +0900

    chore: :card_file_box: add seeders/data

---
+++ src/database/seeders/data.ts
@@ -0,0 +1,7 @@
+export const USER_COUNT = 10;
+export const SELLER_COUNT = 10;
+export const BRAND_COUNT = 10;
+export const ITEM_MAJOR_CATEGORY_COUNT = 5;
+export const ITEM_MINOR_CATEGORY_COUNT = 10;
+export const ITEM_COUNT = 20;
+export const COURIER_COUNT = 10;
