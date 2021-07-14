5678bfc2cffe4d9707052058d15fadb051c32b4e
greatSumini <greatSumini@gmail.com>
2021-07-14 14:17:50 +0900

    feat(application): :sparkles: add OrdersResolver.prepareOrder
    - 입력된 정보를 기반으로 OrderSheet을 찾아낸 뒤, 재고 검사 및 차감을 수행합니다.

--- src/modules/order/order.module.ts
+++ src/modules/order/order.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 
 import { CouponsModule } from './coupons/coupons.module';
 import { OrderSheetsModule } from './order-sheets/order-sheets.module';
+import { OrdersModule } from './orders/orders.module';
 import { PointsModule } from './points/points.module';
 
 @Module({
-  imports: [CouponsModule, OrderSheetsModule, PointsModule],
+  imports: [CouponsModule, OrderSheetsModule, OrdersModule, PointsModule],
 })
 export class OrderModule {}
