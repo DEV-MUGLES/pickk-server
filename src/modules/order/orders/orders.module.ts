9412ebbf5ee336732d030b8bc45a82c4967687be
Yang <ybh1760@gmail.com>
2021-07-21 18:28:27 +0900

    fix(domain): :bug: fix order module imports error

--- src/modules/order/orders/orders.module.ts
+++ src/modules/order/orders/orders.module.ts
@@ -1,11 +1,17 @@
+import { ProductsModule } from '@item/products/products.module';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { OrderSheetsModule } from '@order/order-sheets/order-sheets.module';
 
 import { OrdersRepository } from './orders.repository';
 import { OrdersResolver } from './orders.resolver';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OrdersRepository])],
+  imports: [
+    TypeOrmModule.forFeature([OrdersRepository]),
+    OrderSheetsModule,
+    ProductsModule,
+  ],
   providers: [OrdersResolver],
 })
 export class OrdersModule {}
