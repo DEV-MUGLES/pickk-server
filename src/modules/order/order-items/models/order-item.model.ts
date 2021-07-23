7f5b2602b87bff2419e56641519c04830441d295
greatSumini <greatSumini@gmail.com>
2021-07-23 16:04:21 +0900

    perf(domain): :zap: add id field to Order,OrderItem
    - for optimize apollo-client

--- src/modules/order/order-items/models/order-item.model.ts
+++ src/modules/order/order-items/models/order-item.model.ts
@@ -6,6 +6,14 @@ import { OrderItemEntity } from '../entities/order-item.entity';
 
 @ObjectType()
 export class OrderItem extends OrderItemEntity {
+  @Field({
+    description:
+      'ApolloClient 최적화를 위한 필드입니다. DB에는 존재하지 않습니다.',
+  })
+  get id(): string {
+    return this.merchantUid;
+  }
+
   @Field({ name: 'Order' })
   order: Order;
 }
--- src/modules/order/orders/models/order.model.ts
+++ src/modules/order/orders/models/order.model.ts
@@ -6,6 +6,14 @@ import { OrderEntity } from '../entities';
 
 @ObjectType()
 export class Order extends OrderEntity {
+  @Field({
+    description:
+      'ApolloClient 최적화를 위한 필드입니다. DB에는 존재하지 않습니다.',
+  })
+  get id(): string {
+    return this.merchantUid;
+  }
+
   @Field(() => [OrderItem])
   orderItems: OrderItem[];
 }
