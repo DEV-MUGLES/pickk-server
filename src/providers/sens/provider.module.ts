ec4ee7f4a6f53035f318d91bd0b97d41448f0de7
greatSumini <greatSumini@gmail.com>
2021-07-28 15:36:54 +0900

    feat(application): :sparkles: add SensService

--- src/providers/sens/provider.module.ts
+++ src/providers/sens/provider.module.ts
@@ -1,25 +1,31 @@
-import { Module } from '@nestjs/common';
-import { SensModule, SensModuleAsyncOptions } from 'nest-sens';
+import { Global, Module } from '@nestjs/common';
+import {
+  SensModule,
+  SensModuleAsyncOptions,
+  SensModuleOptions,
+} from 'nest-sens';
 
 import { SensConfigModule, SensConfigService } from '@config/providers/sens';
 
+@Global()
 @Module({
   imports: [
     SensModule.forRootAsync({
       imports: [SensConfigModule],
-      useFactory: async (sensConfigService: SensConfigService) => ({
-        accessKey: sensConfigService.ncloudAccessKey,
-        secretKey: sensConfigService.ncloudSecretKey,
-        sms: {
-          smsServiceId: sensConfigService.ncloudSmsServiceId,
-          smsSecretKey: sensConfigService.ncloudSmsSecretKey,
-          callingNumber: sensConfigService.ncloudSmsCallingNumber,
-        },
-        alimtalk: {
-          alimtalkServiceId: sensConfigService.ncloudAlimtalkServiceId,
-          plusFriendId: sensConfigService.plusFriendId,
-        },
-      }),
+      useFactory: async (sensConfigService: SensConfigService) =>
+        ({
+          accessKey: sensConfigService.ncloudAccessKey,
+          secretKey: sensConfigService.ncloudSecretKey,
+          sms: {
+            smsServiceId: sensConfigService.ncloudSmsServiceId,
+            smsSecretKey: sensConfigService.ncloudSmsSecretKey,
+            callingNumber: sensConfigService.ncloudSmsCallingNumber,
+          },
+          alimtalk: {
+            alimtalkServiceId: sensConfigService.ncloudAlimtalkServiceId,
+            plusFriendId: sensConfigService.plusFriendId,
+          },
+        } as SensModuleOptions),
       inject: [SensConfigService],
     } as SensModuleAsyncOptions),
   ],
--- src/providers/sens/provider.service.ts
+++ src/providers/sens/provider.service.ts
@@ -1,4 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { AlimtalkClient, SmsClient } from 'nest-sens';
 
 @Injectable()
-export class SensService {}
+export class SensService {
+  constructor(
+    @Inject(AlimtalkClient) private readonly alimtalkClient: AlimtalkClient,
+    @Inject(SmsClient) private readonly smsClient: SmsClient
+  ) {}
+
+  async sendSms(to: string | string[], content: string) {
+    await this.smsClient.send({ to, content });
+  }
+}
