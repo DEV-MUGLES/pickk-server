b41458993e8faf57e7ce694ffe09552f4b164e05
Yang <ybh1760@gmail.com>
2021-08-05 11:08:48 +0900

    chore(application): add item-category data

---
+++ src/modules/item/item-categories/constants/item-category.constant.ts
@@ -0,0 +1,242 @@
+export const ItemCategoryData: {
+  name: string;
+  code: string;
+  minorCategories: { name: string; code: string }[];
+}[] = [
+  {
+    name: '아우터',
+    code: 'OUTER',
+    minorCategories: [
+      {
+        code: 'LEATHER_JACKET',
+        name: '레더자켓',
+      },
+      {
+        code: 'BLAZER',
+        name: '블레이저',
+      },
+      {
+        code: 'TRENCH_COAT',
+        name: '트렌치코트',
+      },
+      {
+        code: 'CARDIGAN',
+        name: '가디건',
+      },
+      {
+        code: 'COAT',
+        name: '코트',
+      },
+      {
+        code: 'JACKET',
+        name: '자켓',
+      },
+      {
+        code: 'JUMPER',
+        name: '점퍼',
+      },
+      {
+        code: 'PADDING',
+        name: '패딩',
+      },
+      {
+        code: 'VEST',
+        name: '베스트',
+      },
+    ],
+  },
+  {
+    name: '상의',
+    code: 'TOP',
+    minorCategories: [
+      {
+        code: 'SHORT_TSHIRT',
+        name: '반팔 티셔츠',
+      },
+      {
+        code: 'LONG_TSHIRT',
+        name: '긴팔 티셔츠',
+      },
+      {
+        code: 'SHIRT',
+        name: '셔츠/남방',
+      },
+      {
+        code: 'KNIT',
+        name: '니트',
+      },
+      {
+        code: 'SWEATSHIRT',
+        name: '맨투맨',
+      },
+      {
+        code: 'HOOD',
+        name: '후드',
+      },
+      {
+        code: 'SLEEVELESS',
+        name: '민소매',
+      },
+    ],
+  },
+  {
+    name: '하의',
+    code: 'BOTTOM',
+    minorCategories: [
+      {
+        code: 'DENIM_PANTS',
+        name: '청바지',
+      },
+      {
+        code: 'COTTON_PANTS',
+        name: '면바지',
+      },
+      {
+        code: 'TRAINING_PANTS',
+        name: '트레이닝',
+      },
+      {
+        code: 'SHORT_PANTS',
+        name: '반바지',
+      },
+      {
+        code: 'OVERALLS',
+        name: '멜빵바지',
+      },
+      {
+        code: 'SLACKS',
+        name: '슬랙스',
+      },
+    ],
+  },
+  {
+    name: '신발',
+    code: 'SHOES',
+    minorCategories: [
+      {
+        code: 'SNEAKERS',
+        name: '스니커즈/캔버스',
+      },
+      {
+        code: 'RUNNING_SHOES',
+        name: '운동화',
+      },
+      {
+        code: 'DRESS_SHOES',
+        name: '구두',
+      },
+      {
+        code: 'BOOTS',
+        name: '부츠',
+      },
+      {
+        code: 'SANDAL',
+        name: '샌들',
+      },
+      {
+        code: 'SLIPPER',
+        name: '슬리퍼',
+      },
+      {
+        code: 'DERBY_DRESS_SHOES',
+        name: '더비',
+      },
+      {
+        code: 'LOAFER_DRESS_SHOES',
+        name: '로퍼',
+      },
+      {
+        code: 'CHELSEA_BOOTS',
+        name: '첼시',
+      },
+      {
+        code: 'WALKER_BOOTS',
+        name: '워커',
+      },
+      {
+        code: 'ETC_BOOTS',
+        name: '기타',
+      },
+    ],
+  },
+  {
+    name: '가방',
+    code: 'BAG',
+    minorCategories: [
+      {
+        code: 'BACKPACK',
+        name: '백팩',
+      },
+      {
+        code: 'SHOULDER_BAG',
+        name: '숄더백',
+      },
+      {
+        code: 'CROSS_BAG',
+        name: '크로스백',
+      },
+      {
+        code: 'CARRIER',
+        name: '캐리어',
+      },
+      {
+        code: 'WAIST_BAG',
+        name: '웨이스트백',
+      },
+      {
+        code: 'CLUTCH_POUCH',
+        name: '클러치/파우치',
+      },
+      {
+        code: 'ETC_BAG',
+        name: '기타',
+      },
+      {
+        code: 'BOSTON_BAG',
+        name: '보스턴백',
+      },
+    ],
+  },
+  {
+    name: '악세서리',
+    code: 'ACCESSORY',
+    minorCategories: [
+      {
+        code: 'HAT',
+        name: '모자',
+      },
+      {
+        code: 'WATCH',
+        name: '시계',
+      },
+      {
+        code: 'WALLET',
+        name: '지갑',
+      },
+      {
+        code: 'BELT',
+        name: '벨트',
+      },
+      {
+        code: 'GLASSES',
+        name: '안경',
+      },
+      {
+        code: 'SUNGLASSES',
+        name: '선글라스',
+      },
+      {
+        code: 'MUFFLER',
+        name: '머플러',
+      },
+      {
+        code: 'JEWELRY',
+        name: '쥬얼리',
+      },
+      {
+        code: 'ETC_ACCESSORY',
+        name: '기타',
+      },
+    ],
+  },
+];
