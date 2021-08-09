59f0053010a9a7e6d9f3539ebac9ad68754f9ab9
greatSumini <greatSumini@gmail.com>
2021-08-10 03:24:26 +0900

    feat(provider): :sparkles: add AlimtalkService.sendRefundRequested

--- src/providers/sens/provider.alimtalk.service.ts
+++ src/providers/sens/provider.alimtalk.service.ts
@@ -1,9 +1,19 @@
 import { Inject, Injectable } from '@nestjs/common';
 import { AlimtalkClient } from 'nest-sens';
 
+import { RefundRequestedTemplate } from '@templates/alimtalk';
+
+import { RefundRequest } from '@order/refund-requests/models';
+
 @Injectable()
 export class AlimtalkService {
   constructor(
     @Inject(AlimtalkClient) private readonly alimtalkClient: AlimtalkClient
   ) {}
+
+  async sendRefundRequested(refundRequest: RefundRequest) {
+    await this.alimtalkClient.send(
+      RefundRequestedTemplate.toRequest(refundRequest)
+    );
+  }
 }
