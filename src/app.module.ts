bcddcfc0a62f262c487ac1fea04dd25a26bed7e7
Yang <ybh1760@gmail.com>
2021-08-09 12:19:54 +0900

    fix: resolve circular dependency

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,27 +1,27 @@
 import { Module } from '@nestjs/common';
 import { Connection } from 'typeorm';
-
 import { GraphQLModule } from '@nestjs/graphql';
-import { AppConfigModule } from './config/app/config.module';
-import { AwsS3ProviderModule } from './providers/aws/s3/provider.module';
+
+import { AuthModule } from '@auth/auth.module';
+import { BatchModule } from '@batch/batch.module';
+import { AppConfigModule } from '@config/app/config.module';
+import { CommonModule } from '@mcommon/common.module';
+import { ItemModule } from '@item/item.module';
+import { MyModule } from '@my/my.module';
+import { UserModule } from '@user/user.module';
+import { OrderModule } from '@order/order.module';
+import { PaymentModule } from '@payment/payment.module';
+
+import { AwsS3ProviderModule } from '@providers/aws/s3/provider.module';
 import { AwsSqsProviderModule } from '@providers/aws/sqs/provider.module';
-import { RedisCacheProviderModule } from './providers/cache/redis';
-import { MysqlDatabaseProviderModule } from './providers/database/mysql/provider.module';
-import { SensProviderModule } from './providers/sens/provider.module';
-import { SpiderModule } from './providers/spider/provider.module';
-
-import { AuthModule } from './auth/auth.module';
-
-import { CommonModule } from './modules/common/common.module';
-import { ItemModule } from './modules/item/item.module';
-import { MyModule } from './modules/my/my.module';
-import { UserModule } from './modules/user/user.module';
-import { OrderModule } from './modules/order/order.module';
+import { RedisCacheProviderModule } from '@providers/cache/redis';
+import { MysqlDatabaseProviderModule } from '@providers/database/mysql/provider.module';
+import { SensProviderModule } from '@providers/sens/provider.module';
+import { SpiderModule } from '@providers/spider/provider.module';
+import { CrawlerProviderModule } from '@providers/crawler/provider.module';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PaymentModule } from '@payment/payment.module';
-import { BatchModule } from './batch/batch.module';
 
 @Module({
   imports: [
@@ -47,6 +47,7 @@ import { BatchModule } from './batch/batch.module';
     PaymentModule,
     UserModule,
     BatchModule,
+    CrawlerProviderModule,
   ],
   controllers: [AppController],
   providers: [AppService],
