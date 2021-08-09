8190b214f395e29618e31cfeef8828cfac23435f
Yang <ybh1760@gmail.com>
2021-08-09 10:47:28 +0900

    feat: add isEqualSet array helper and splitRegexString string helper

--- src/common/helpers/array.helpers.ts
+++ src/common/helpers/array.helpers.ts
@@ -1,2 +1,5 @@
 export const isAllEleSame = (arr: unknown[]): boolean =>
   [...new Set(arr)].length <= 1;
+
+export const isEqualSet = (a: Set<unknown>, b: Set<unknown>): boolean =>
+  a.size === b.size && [...a].every((value) => b.has(value));
--- src/common/helpers/index.ts
+++ src/common/helpers/index.ts
@@ -1,3 +1,4 @@
 export * from './array.helpers';
 export * from './filter.helpers';
 export * from './random.helpers';
+export * from './string.helpers';
