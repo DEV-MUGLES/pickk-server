2592dd082c69e66bf3bf0bbb3fa389112c4ec8d4
Yang <ybh1760@gmail.com>
2021-08-09 13:31:21 +0900

    chore(domain): :fire: remove SellerCrawlStrategy starturls getter

--- src/modules/item/sellers/models/seller-crawl-strategy.model.ts
+++ src/modules/item/sellers/models/seller-crawl-strategy.model.ts
@@ -3,10 +3,4 @@ import { ObjectType } from '@nestjs/graphql';
 import { SellerCrawlStrategyEntity } from '../entities';
 
 @ObjectType()
-export class SellerCrawlStrategy extends SellerCrawlStrategyEntity {
-  get startUrls(): string[] {
-    return this.startPathNamesJoin
-      .split('|||')
-      .map((pathName) => this.baseUrl + pathName);
-  }
-}
+export class SellerCrawlStrategy extends SellerCrawlStrategyEntity {}
