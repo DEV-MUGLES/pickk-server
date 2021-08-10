37f315e99615dc359131b2aeed09ca2d0306e8b4
greatSumini <greatSumini@gmail.com>
2021-08-10 16:43:59 +0900

    feat(admin): :sparkles: add SellerOrderItemResolver.meSellerOrderItemsCount

--- src/modules/admin/seller/order/seller-order-admin.module.ts
+++ src/modules/admin/seller/order/seller-order-admin.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { OrderItemsModule } from '@order/order-items/order-items.module';
+import { OrderItemsRepository } from '@order/order-items/order-items.repository';
 
 import { SellerOrderItemResolver } from './seller-order-item.resolver';
 
 @Module({
-  imports: [OrderItemsModule],
+  imports: [TypeOrmModule.forFeature([OrderItemsRepository]), OrderItemsModule],
   providers: [SellerOrderItemResolver],
 })
 export class SellerOrderAdminModule {}
--- src/modules/admin/seller/order/seller-order-item.resolver.ts
+++ src/modules/admin/seller/order/seller-order-item.resolver.ts
@@ -1,12 +1,16 @@
 import { Inject, Injectable, UseGuards } from '@nestjs/common';
 import { Args, Info, Query } from '@nestjs/graphql';
+import { InjectRepository } from '@nestjs/typeorm';
+import { MoreThanOrEqual } from 'typeorm';
 import { GraphQLResolveInfo } from 'graphql';
+import dayjs from 'dayjs';
 
 import { CurrentUser } from '@auth/decorators';
 import { JwtSellerVerifyGuard } from '@auth/guards';
 import { JwtPayload } from '@auth/models';
 import { PageInput } from '@common/dtos';
 import { BaseResolver } from '@common/base.resolver';
+import { CacheService } from '@providers/cache/redis';
 
 import {
   OrderItemRelationType,
@@ -14,7 +18,10 @@ import {
 } from '@order/order-items/constants';
 import { OrderItemFilter } from '@order/order-items/dtos';
 import { OrderItem } from '@order/order-items/models';
+import { OrderItemsRepository } from '@order/order-items/order-items.repository';
 import { OrderItemsService } from '@order/order-items/order-items.service';
+
+import { OrderItemsCountOutput } from './dtos';
 
 @Injectable()
 export class SellerOrderItemResolver extends BaseResolver<OrderItemRelationType> {
@@ -22,7 +29,10 @@ export class SellerOrderItemResolver extends BaseResolver<OrderItemRelationType>
 
   constructor(
     @Inject(OrderItemsService)
-    private readonly orderItemsService: OrderItemsService
+    private readonly orderItemsService: OrderItemsService,
+    @InjectRepository(OrderItemsRepository)
+    private readonly orderItemsRepository: OrderItemsRepository,
+    @Inject(CacheService) private cacheService: CacheService
   ) {
     super();
   }
@@ -44,5 +54,40 @@ export class SellerOrderItemResolver extends BaseResolver<OrderItemRelationType>
       pageInput,
       this.getRelationsFromInfo(info)
     );
+  }
+
+  @Query(() => OrderItemsCountOutput)
+  @UseGuards(JwtSellerVerifyGuard)
+  async meSellerOrderItemsCount(
+    @CurrentUser() { sellerId }: JwtPayload,
+    @Args('forceUpdate', { nullable: true }) forceUpdate?: boolean
+  ): Promise<OrderItemsCountOutput> {
+    if (!forceUpdate) {
+      const cached = await this.cacheService.get<OrderItemsCountOutput>(
+        OrderItemsCountOutput.getCacheKey(sellerId)
+      );
+      cached.lastUpdatedAt = new Date(cached.lastUpdatedAt);
+
+      if (cached) {
+        return cached;
+      }
+    }
+
+    const orderItems = await this.orderItemsRepository.find({
+      select: ['status', 'claimStatus'],
+      where: {
+        sellerId,
+        createdAt: MoreThanOrEqual(dayjs().subtract(1, 'month').toDate()),
+      },
+    });
+
+    const result = OrderItemsCountOutput.create(sellerId, orderItems);
+    await this.cacheService.set<OrderItemsCountOutput>(
+      result.cacheKey,
+      result,
+      { ttl: 60 * 5 }
+    );
+
+    return result;
   }
 }
