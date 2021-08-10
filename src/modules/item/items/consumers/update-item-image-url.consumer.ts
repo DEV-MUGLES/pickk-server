c0fb4a0ae67f62a7c1fe6412eae70a0081d3aa69
Yang <ybh1760@gmail.com>
2021-08-10 19:54:56 +0900

    enhance(application): :art: refactor UpdateItemImageUrlConsumer

--- src/modules/item/items/consumers/update-item-image-url.consumer.ts
+++ src/modules/item/items/consumers/update-item-image-url.consumer.ts
@@ -1,38 +1,46 @@
 import { HttpService } from '@nestjs/axios';
-import { firstValueFrom } from 'rxjs';
+import { Logger } from '@nestjs/common';
 import {
   SqsConsumerEvent,
   SqsConsumerEventHandler,
   SqsMessageHandler,
   SqsProcess,
 } from '@pickk/nestjs-sqs';
+import { firstValueFrom } from 'rxjs';
 
 import { getMimeType } from '@src/modules/common/images/helpers';
 import { ImagesService } from '@src/modules/common/images/images.service';
+import { UPDATE_ITEM_IMAGE_URL_QUEUE } from '@src/queue/constants';
+import { UpdateItemImageUrlMto } from '@src/queue/mtos';
 import { ItemsService } from '@item/items/items.service';
-
-import { UPDATE_ITEM_IMAGE_URL_QUEUE } from '../constants';
-import { UpdateItemImageUrlMto } from '../mtos';
 
 @SqsProcess(UPDATE_ITEM_IMAGE_URL_QUEUE)
 export class UpdateItemImageUrlConsumer {
   constructor(
     private readonly httpService: HttpService,
     private readonly imagesService: ImagesService,
-    private readonly itemsService: ItemsService
+    private readonly itemsService: ItemsService,
+    private readonly logger: Logger
   ) {}
 
   @SqsMessageHandler()
   async update(message: AWS.SQS.Message): Promise<void> {
     const { Body } = message;
     const messageData: UpdateItemImageUrlMto = JSON.parse(Body);
-    const { itemId, imageUrl } = messageData;
+    const { brandId, code, imageUrl, itemId } = messageData;
+
+    this.validateMessageData(messageData);
 
     const { data } = await firstValueFrom(
       this.httpService.get<Buffer>(imageUrl, {
         responseType: 'arraybuffer',
       })
     );
+    const findOption = itemId
+      ? { id: itemId }
+      : { brandId, providedCode: code };
+
+    const item = await this.itemsService.findOne(findOption);
 
     const mimetype = getMimeType(imageUrl);
     const [result] = await this.imagesService.uploadBufferDatas([
@@ -40,18 +48,30 @@ export class UpdateItemImageUrlConsumer {
         buffer: data,
         filename: `THUMBNAIL.${mimetype}`,
         mimetype: mimetype,
-        prefix: `item/${itemId}`,
+        prefix: `item/${item.id}`,
       },
     ]);
 
-    const item = await this.itemsService.get(itemId);
     await this.itemsService.update(item, {
       imageUrl: result.url,
     });
   }
 
+  validateMessageData(messageData: UpdateItemImageUrlMto) {
+    const { itemId, brandId, code } = messageData;
+
+    if (itemId === undefined && brandId === undefined) {
+      throw new Error(
+        'itemId가 undefined이면, brandId와 code를 입력받아야 합니다'
+      );
+    }
+    if (brandId === undefined || code === undefined) {
+      throw new Error('brandId와 code 둘 다 입력받아야 합니다.');
+    }
+  }
+
   @SqsConsumerEventHandler(SqsConsumerEvent.PROCESSING_ERROR)
-  processingError(error) {
-    console.log(error);
+  processingError(error: Error) {
+    this.logger.error(error);
   }
 }
