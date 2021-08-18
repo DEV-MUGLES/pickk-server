277358a2af5455e8033e892572e49d518f00022d
greatSumini <greatSumini@gmail.com>
2021-08-18 16:21:34 +0900

    feat(application): :sparkles: add digest resolver, service

---
+++ src/modules/content/digests/constants/digest.relation.ts
@@ -0,0 +1,12 @@
+import { Digest } from '../models';
+
+export type DigestRelationType = keyof Digest;
+
+export const DIGEST_RELATIONS: DigestRelationType[] = [
+  'item',
+  'user',
+  'video',
+  'look',
+  'itemPropertyValues',
+  'comments',
+];
