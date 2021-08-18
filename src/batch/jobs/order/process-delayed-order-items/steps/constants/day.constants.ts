6515bc51f112cf13d56a9112cc703a5482a4baa0
Yang <ybh1760@gmail.com>
2021-08-18 12:12:30 +0900

    feat(job): change oldDay value to 2020-01-01

--- src/batch/jobs/order/process-delayed-order-items/steps/constants/day.constants.ts
+++ src/batch/jobs/order/process-delayed-order-items/steps/constants/day.constants.ts
@@ -1,4 +1,4 @@
 import dayjs from 'dayjs';
 
-/** null인 날짜에 대해서는 null 대신 사용되는 값입니다. 현재 날짜로부터 일년전 날짜를 반환합니다.*/
-export const oldDay = dayjs().add(-1, 'year');
+/** null인 날짜에 대해서는 null 대신 사용되는 값입니다. 2000-01-01의 날짜를 값으로 가집니다.*/
+export const oldDay = dayjs(new Date(2000, 1, 1));
