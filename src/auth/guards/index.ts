f723a64d260452f43adb4f766c1b69e316786d11
greatSumini <greatSumini@gmail.com>
2021-08-20 03:04:39 +0900

    feat(auth): add JwtOrIpGuard
    - jwt payload 또는 ip string를 반환합니다

--- src/auth/guards/index.ts
+++ src/auth/guards/index.ts
@@ -4,5 +4,6 @@ export * from './jwt-auth.guard';
 export * from './jwt-refresh.guard';
 export * from './jwt-seller-verify.guard';
 export * from './jwt-seller.guard';
+export * from './jwt-or-ip.guard';
 export * from './jwt-verify.guard';
 export * from './super-secret.guard';
