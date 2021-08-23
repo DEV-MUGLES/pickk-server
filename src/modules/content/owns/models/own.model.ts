0c2f3e36656434a6bcccfcd15cf6432af2e60c6e
greatSumini <greatSumini@gmail.com>
2021-08-24 04:39:43 +0900

    feat(domain): add countCacheKey to Own

--- src/modules/content/owns/models/own.model.ts
+++ src/modules/content/owns/models/own.model.ts
@@ -3,4 +3,8 @@ import { ObjectType } from '@nestjs/graphql';
 import { OwnEntity } from '../entities';
 
 @ObjectType()
-export class Own extends OwnEntity {}
+export class Own extends OwnEntity {
+  static countCacheKey(userId: number, keywordClassId: number): string {
+    return `user:${userId}:keywordClass:${keywordClassId}:own`;
+  }
+}
