f51bda8748d0076dfe5e74c3a82406982d253e71
greatSumini <greatSumini@gmail.com>
2021-08-25 02:39:36 +0900

    feat(infra): update LOOK_RELATIONS, VIDEO_RELATIONS
    - add styleTags(look), images(look), digests.item, digests.item.brand, digests.item.prices

--- src/modules/content/looks/constants/look.relation.ts
+++ src/modules/content/looks/constants/look.relation.ts
@@ -1,5 +1,17 @@
 import { Look } from '../models';
 
-export type LookRelationType = keyof Look;
+export type LookRelationType =
+  | keyof Look
+  | 'digests.item'
+  | 'digests.item.brand'
+  | 'digests.item.prices';
 
-export const LOOK_RELATIONS: LookRelationType[] = ['user', 'digests'];
+export const LOOK_RELATIONS: LookRelationType[] = [
+  'user',
+  'styleTags',
+  'images',
+  'digests',
+  'digests.item',
+  'digests.item.brand',
+  'digests.item.prices',
+];
--- src/modules/content/videos/constants/video.relation.ts
+++ src/modules/content/videos/constants/video.relation.ts
@@ -1,5 +1,15 @@
 import { Video } from '../models';
 
-export type VideoRelationType = keyof Video;
+export type VideoRelationType =
+  | keyof Video
+  | 'digests.item'
+  | 'digests.item.brand'
+  | 'digests.item.prices';
 
-export const VIDEO_RELATIONS: VideoRelationType[] = ['user', 'digests'];
+export const VIDEO_RELATIONS: VideoRelationType[] = [
+  'user',
+  'digests',
+  'digests.item',
+  'digests.item.brand',
+  'digests.item.prices',
+];
