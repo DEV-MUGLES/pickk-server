3d716c08aced07c69cc556ed46226b2b85de38bc
greatSumini <greatSumini@gmail.com>
2021-08-25 04:54:58 +0900

    enhance(application): make KeywordFilter.keywordClassId required

--- src/modules/content/keywords/dtos/keyword.filter.ts
+++ src/modules/content/keywords/dtos/keyword.filter.ts
@@ -4,7 +4,7 @@ import { IKeyword } from '../interfaces';
 
 @InputType()
 export class KeywordFilter implements Partial<IKeyword> {
-  excludeFields: Array<keyof IKeyword | 'keywordClassId'> = [
+  excludeFields: Array<keyof KeywordFilter> = [
     'keywordClassId',
     'isOwning',
     'isLiking',
@@ -13,13 +13,20 @@ export class KeywordFilter implements Partial<IKeyword> {
   @Field({ nullable: true })
   isVisible: boolean;
 
-  @Field(() => Int, {
-    description: '제공시 추가 연산을 수행합니다.',
+  @Field(() => Int, { description: '제공시 추가 연산을 수행합니다.' })
+  keywordClassId: number;
+  @Field({
+    description: '제공시 추가 연산을 수행합니다. (비로그인시 무시)',
     nullable: true,
   })
-  keywordClassId: number;
-  @Field({ description: '제공시 추가 연산을 수행합니다.', nullable: true })
   isOwning: boolean;
-  @Field({ description: '제공시 추가 연산을 수행합니다.', nullable: true })
+  @Field({
+    description: '제공시 추가 연산을 수행합니다. (비로그인시 무시)',
+    nullable: true,
+  })
   isLiking: boolean;
+
+  get hasCustom() {
+    return this.excludeFields.some((field) => this[field]);
+  }
 }
