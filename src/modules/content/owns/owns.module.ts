6b0139c472be99212e841450bcb60a4dee2d0fb0
Yang <ybh1760@gmail.com>
2021-08-24 18:17:42 +0900

    feat(domain): :art: enroll OwningCountCacheProducer and UpdateKeywordClassOwningCountCacheConsumer to OwnsModule

--- src/modules/content/owns/owns.module.ts
+++ src/modules/content/owns/owns.module.ts
@@ -1,14 +1,30 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { SqsModule } from '@pickk/nestjs-sqs';
 
 import { KeywordsModule } from '@content/keywords/keywords.module';
+import { UPDATE_KEYWORD_CLASS_OWNING_COUNT_CACHE_QUEUE } from '@queue/constants';
+
+import { UpdateKeywordClassOwningCountCacheConsumer } from './consumers';
+import { OwningCountCacheProducer } from './producers';
 
 import { OwnsRepository } from './owns.repository';
 import { OwnsResolver } from './owns.resolver';
 import { OwnsService } from './owns.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([OwnsRepository]), KeywordsModule],
-  providers: [OwnsResolver, OwnsService],
+  imports: [
+    TypeOrmModule.forFeature([OwnsRepository]),
+    KeywordsModule,
+    SqsModule.registerQueue({
+      name: UPDATE_KEYWORD_CLASS_OWNING_COUNT_CACHE_QUEUE,
+    }),
+  ],
+  providers: [
+    OwnsResolver,
+    OwnsService,
+    OwningCountCacheProducer,
+    UpdateKeywordClassOwningCountCacheConsumer,
+  ],
 })
 export class OwnsModule {}
