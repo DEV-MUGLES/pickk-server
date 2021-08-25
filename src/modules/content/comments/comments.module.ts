518c98915d08e51077e1e96237a7e938d8f97f05
Yang <ybh1760@gmail.com>
2021-08-25 19:42:14 +0900

    feat(queue): :sparkles: add CommentsProducer

--- src/modules/content/comments/comments.module.ts
+++ src/modules/content/comments/comments.module.ts
@@ -3,9 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SqsModule, SqsQueueType } from '@pickk/nestjs-sqs';
 
 import { LikesModule } from '@content/likes/likes.module';
-import { UPDATE_COMMENT_LIKE_COUNT_QUEUE } from '@queue/constants';
+import {
+  UPDATE_COMMENT_LIKE_COUNT_QUEUE,
+  UPDATE_DIGEST_COMMENT_COUNT_QUEUE,
+  UPDATE_LOOK_COMMENT_COUNT_QUEUE,
+  UPDATE_VIDEO_COMMENT_COUNT_QUEUE,
+} from '@queue/constants';
 
 import { UpdateCommentLikeCountConsumer } from './consumers';
+import { CommentsProducer } from './producers';
 
 import { CommentsRepository } from './comments.repository';
 import { CommentsResolver } from './comments.resolver';
@@ -14,17 +20,32 @@ import { CommentsService } from './comments.service';
 @Module({
   imports: [
     TypeOrmModule.forFeature([CommentsRepository]),
-    SqsModule.registerQueue({
-      name: UPDATE_COMMENT_LIKE_COUNT_QUEUE,
-      type: SqsQueueType.Consumer,
-      consumerOptions: { batchSize: 10 },
-    }),
+    SqsModule.registerQueue(
+      {
+        name: UPDATE_COMMENT_LIKE_COUNT_QUEUE,
+        type: SqsQueueType.Consumer,
+        consumerOptions: { batchSize: 10 },
+      },
+      {
+        name: UPDATE_DIGEST_COMMENT_COUNT_QUEUE,
+        type: SqsQueueType.Producer,
+      },
+      {
+        name: UPDATE_LOOK_COMMENT_COUNT_QUEUE,
+        type: SqsQueueType.Producer,
+      },
+      {
+        name: UPDATE_VIDEO_COMMENT_COUNT_QUEUE,
+        type: SqsQueueType.Producer,
+      }
+    ),
     LikesModule,
   ],
   providers: [
     CommentsResolver,
     CommentsService,
     UpdateCommentLikeCountConsumer,
+    CommentsProducer,
   ],
 })
 export class CommentsModule {}
---
+++ src/modules/content/comments/producers/comments.producer.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { SqsService } from '@pickk/nestjs-sqs';
+
+import {
+  UPDATE_DIGEST_COMMENT_COUNT_QUEUE,
+  UPDATE_LOOK_COMMENT_COUNT_QUEUE,
+  UPDATE_VIDEO_COMMENT_COUNT_QUEUE,
+} from '@queue/constants';
+import { UpdateCommentCountMto } from '@queue/mtos';
+
+import { CommentOwnerType } from '../constants';
+
+@Injectable()
+export class CommentsProducer {
+  constructor(private readonly sqsService: SqsService) {}
+
+  async updateOwnerCommentCount(ownerId: number, ownerType: CommentOwnerType) {
+    await this.sqsService.send<UpdateCommentCountMto>(
+      this.getQueueNameByOwnerType(ownerType),
+      {
+        id: ownerId.toString(),
+        body: { id: ownerId },
+      }
+    );
+  }
+
+  private getQueueNameByOwnerType(ownerType: CommentOwnerType) {
+    if (ownerType === CommentOwnerType.Digest) {
+      return UPDATE_DIGEST_COMMENT_COUNT_QUEUE;
+    }
+    if (ownerType === CommentOwnerType.Look) {
+      return UPDATE_LOOK_COMMENT_COUNT_QUEUE;
+    }
+    if (ownerType === CommentOwnerType.Video) {
+      return UPDATE_VIDEO_COMMENT_COUNT_QUEUE;
+    }
+  }
+}
