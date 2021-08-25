1716566b698952d4455b51ca1d6badda8df7e66f
Yang <ybh1760@gmail.com>
2021-08-25 19:51:39 +0900

    feat(queue): :sparkles: add UpdateLookCommentCountConsumer

--- src/modules/content/looks/consumers/index.ts
+++ src/modules/content/looks/consumers/index.ts
@@ -1,1 +1,2 @@
 export * from './update-look-like-count.consumer';
+export * from './update-look-comment-count.consumer';
--- src/modules/content/looks/looks.module.ts
+++ src/modules/content/looks/looks.module.ts
@@ -3,9 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SqsModule, SqsQueueType } from '@pickk/nestjs-sqs';
 
 import { LikesModule } from '@content/likes/likes.module';
-import { UPDATE_LOOK_LIKE_COUNT_QUEUE } from '@queue/constants';
+import { CommentsRepository } from '@content/comments/comments.repository';
+import {
+  UPDATE_LOOK_LIKE_COUNT_QUEUE,
+  UPDATE_LOOK_COMMENT_COUNT_QUEUE,
+} from '@queue/constants';
 
-import { UpdateLookLikeCountConsumer } from './consumers';
+import {
+  UpdateLookCommentCountConsumer,
+  UpdateLookLikeCountConsumer,
+} from './consumers';
 
 import { LooksRepository } from './looks.repository';
 import { LooksResolver } from './looks.resolver';
@@ -13,14 +20,26 @@ import { LooksService } from './looks.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([LooksRepository]),
-    SqsModule.registerQueue({
-      name: UPDATE_LOOK_LIKE_COUNT_QUEUE,
-      type: SqsQueueType.Consumer,
-      consumerOptions: { batchSize: 10 },
-    }),
+    TypeOrmModule.forFeature([LooksRepository, CommentsRepository]),
+    SqsModule.registerQueue(
+      {
+        name: UPDATE_LOOK_LIKE_COUNT_QUEUE,
+        type: SqsQueueType.Consumer,
+        consumerOptions: { batchSize: 10 },
+      },
+      {
+        name: UPDATE_LOOK_COMMENT_COUNT_QUEUE,
+        type: SqsQueueType.Consumer,
+        consumerOptions: { batchSize: 10 },
+      }
+    ),
     LikesModule,
   ],
-  providers: [LooksResolver, LooksService, UpdateLookLikeCountConsumer],
+  providers: [
+    LooksResolver,
+    LooksService,
+    UpdateLookLikeCountConsumer,
+    UpdateLookCommentCountConsumer,
+  ],
 })
 export class LooksModule {}
