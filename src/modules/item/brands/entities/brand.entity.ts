0023e0067c8876744f822d6a46ccff8cf55bc5a8
greatSumini <greatSumini@gmail.com>
2021-08-26 02:09:36 +0900

    feat(domain): :sparkles: add sellerId field to Brand

--- src/modules/item/brands/entities/brand.entity.ts
+++ src/modules/item/brands/entities/brand.entity.ts
@@ -1,48 +1,29 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { IsOptional, IsUrl, MaxLength } from 'class-validator';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
 import { Column, Entity, OneToOne } from 'typeorm';
 
 import { BaseIdEntity } from '@common/entities';
 import { SellerEntity } from '@item/sellers/entities/seller.entity';
 
-import { IBrand } from '../interfaces/brand.interface';
+import { IBrand } from '../interfaces';
 
 @ObjectType()
 @Entity({ name: 'brand' })
 export class BrandEntity extends BaseIdEntity implements IBrand {
   @Field()
-  @Column({
-    type: 'varchar',
-    length: 30,
-  })
-  @MaxLength(30)
+  @Column({ length: 30 })
   nameKor: string;
-
   @Field({ nullable: true })
-  @Column({
-    type: 'varchar',
-    length: 30,
-    nullable: true,
-  })
-  @MaxLength(30)
-  @IsOptional()
+  @Column({ length: 30, nullable: true })
   nameEng?: string;
-
-  @Field({ nullable: true })
-  @Column({
-    type: 'varchar',
-    nullable: true,
-  })
-  @IsOptional()
-  description?: string;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
-  @IsUrl()
+  description?: string;
+  @Field({ nullable: true })
+  @Column({ nullable: true })
   imageUrl?: string;
 
-  @OneToOne('SellerEntity', 'brand', {
-    nullable: true,
-  })
+  @OneToOne('SellerEntity', 'brand', { nullable: true })
   seller?: SellerEntity;
+  @Field(() => Int, { nullable: true })
+  sellerId?: number;
 }
