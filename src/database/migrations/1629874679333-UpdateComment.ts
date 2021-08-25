6ad1b05cd0ce47c7649ed52140e034a56f2e8f92
Yang <ybh1760@gmail.com>
2021-08-25 17:20:08 +0900

    enhance(infra): :art: combine AddIsDeleted2Comment and UpdateCommentContent2Nullable migrations to UpdateComment migration

--- src/database/migrations/1629874679333-UpdateComment.ts
+++ src/database/migrations/1629874679333-UpdateComment.ts
@@ -1,11 +1,12 @@
 import { MigrationInterface, QueryRunner } from 'typeorm';
 
-export class UpdateCommentContent2Nullable1629876210391
-  implements MigrationInterface
-{
-  name = 'UpdateCommentContent2Nullable1629876210391';
+export class UpdateComment1629874679333 implements MigrationInterface {
+  name = 'UpdateComment1629874679333';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      'ALTER TABLE `comment` ADD `isDeleted` tinyint NOT NULL DEFAULT 0'
+    );
     await queryRunner.query(
       'ALTER TABLE `comment` CHANGE `content` `content` varchar(255) NULL'
     );
@@ -15,5 +16,6 @@ export class UpdateCommentContent2Nullable1629876210391
     await queryRunner.query(
       'ALTER TABLE `comment` CHANGE `content` `content` varchar(255) NOT NULL'
     );
+    await queryRunner.query('ALTER TABLE `comment` DROP COLUMN `isDeleted`');
   }
 }
