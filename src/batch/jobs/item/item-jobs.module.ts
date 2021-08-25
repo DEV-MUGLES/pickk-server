23efa051d2f61abdb8f89e3b187f2ca1dcb22d58
Yang <ybh1760@gmail.com>
2021-08-25 12:36:53 +0900

    enhance: :art: update import statements in item-jobs.module.ts

--- src/batch/jobs/item/item-jobs.module.ts
+++ src/batch/jobs/item/item-jobs.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { BatchWorker } from '@src/batch/batch.worker';
-import { JobsModule } from '@src/modules/common/jobs/jobs.module';
+import { BatchWorker } from '@batch/batch.worker';
+import { JobsModule } from '@mcommon/jobs/jobs.module';
 import { SellersModule } from '@item/sellers/sellers.module';
 import { ProductsRepository } from '@item/products/products.repository';
 import { ItemsRepository } from '@item/items/items.repository';
