954a4a0448eb38704a1b39d5b6e0ba57c4ae0421
greatSumini <greatSumini@gmail.com>
2021-08-25 17:11:03 +0900

    feat(application): :sparkles: add user.heightBetween,orderBy,excludeFields to LookFilter

--- src/modules/content/looks/dtos/look.filter.ts
+++ src/modules/content/looks/dtos/look.filter.ts
@@ -1,14 +1,34 @@
-import { Field, InputType, Int } from '@nestjs/graphql';
+import { Field, InputType, Int, registerEnumType } from '@nestjs/graphql';
 
 import { ILook } from '../interfaces';
 
 @InputType()
-export class LookFilter implements Partial<ILook> {
-  excludeFields: Array<keyof LookFilter> = ['styleTagIdIn'];
+export class LookUserFilter {
+  @Field(() => [Int, Int], { nullable: true })
+  heightBetween: [number, number];
+}
+
+export enum LookOrderBy {
+  Id = 'id',
+  Score = 'score',
+}
+
+registerEnumType(LookOrderBy, {
+  name: 'LookOrderBy',
+});
+
+@InputType()
+export class LookFilter implements Partial<Omit<ILook, 'user'>> {
+  excludeFields: Array<keyof LookFilter> = ['styleTagIdIn', 'orderBy'];
 
   @Field(() => Int, { nullable: true })
   userId: number;
+  @Field(() => LookUserFilter, { nullable: true })
+  user: LookUserFilter;
 
   @Field(() => [Int], { nullable: true })
   styleTagIdIn: number[];
+
+  @Field(() => LookOrderBy, { defaultValue: LookOrderBy.Id })
+  orderBy: LookOrderBy;
 }
