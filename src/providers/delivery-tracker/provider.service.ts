dcdcf57777cbf891960f72aefc743e683ce42a22
Yang <ybh1760@gmail.com>
2021-08-26 21:10:47 +0900

    enhance(provider): :art: rename getTrackInfo to trackHistory and add data parse logic DeliveryTrackerService.trackHistory

--- src/providers/delivery-tracker/provider.service.ts
+++ src/providers/delivery-tracker/provider.service.ts
@@ -4,21 +4,29 @@ import { firstValueFrom } from 'rxjs';
 
 import { TrackDeliveryDto } from './dtos';
 
+import { IShipmentHistory } from '@order/shipments/interfaces';
+
 @Injectable()
 export class DeliveryTrackerService {
   private readonly url = 'https://delivery-track.pickk.one/api/carriers';
 
   constructor(private readonly httpService: HttpService) {}
 
-  async getTrackInfo(
+  async trackHistory(
     courierCode: string,
     trackCode: string
-  ): Promise<TrackDeliveryDto> {
-    const { data } = await firstValueFrom(
+  ): Promise<Pick<IShipmentHistory, 'time' | 'statusText' | 'locationName'>[]> {
+    const {
+      data: { progresses },
+    } = await firstValueFrom(
       this.httpService.get<TrackDeliveryDto>(
         `${this.url}/${courierCode}/tracks/${trackCode}`
       )
     );
-    return data;
+    return progresses.map(({ time, status, location }) => ({
+      time,
+      statusText: status.text,
+      locationName: location.name,
+    }));
   }
 }
