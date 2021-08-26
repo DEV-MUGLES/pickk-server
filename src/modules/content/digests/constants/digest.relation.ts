c3798b1316bcba580170f9022c5a61fb5dba7768
greatSumini <greatSumini@gmail.com>
2021-08-26 19:33:13 +0900

    feat(infra): :card_file_box: add item.brand, item.minorCategory to DIGEST_RELATIONS

--- src/modules/content/digests/constants/digest.relation.ts
+++ src/modules/content/digests/constants/digest.relation.ts
@@ -1,9 +1,14 @@
 import { Digest } from '../models';
 
-export type DigestRelationType = keyof Digest;
+export type DigestRelationType =
+  | keyof Digest
+  | 'item.brand'
+  | 'item.minorCategory';
 
 export const DIGEST_RELATIONS: DigestRelationType[] = [
   'item',
+  'item.brand',
+  'item.minorCategory',
   'user',
   'video',
   'look',
