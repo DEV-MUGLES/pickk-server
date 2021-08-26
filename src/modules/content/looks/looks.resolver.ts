8f93fff0139a38f8d1a002b470c5281763c5616d
greatSumini <greatSumini@gmail.com>
2021-08-27 03:21:15 +0900

    feat(application): :sparkles: add likingLooks query

--- src/modules/content/looks/looks.resolver.ts
+++ src/modules/content/looks/looks.resolver.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { Args, Info, Query } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
 
+import { CurrentUser } from '@auth/decorators';
+import { JwtVerifyGuard } from '@auth/guards';
+import { JwtPayload } from '@auth/models';
 import { PageInput } from '@common/dtos';
 import { BaseResolver } from '@common/base.resolver';
 
 import { LookSearchService } from '@mcommon/search/look.search.service';
+import { LikeOwnerType } from '@content/likes/constants';
+import { LikesService } from '@content/likes/likes.service';
 
 import { LookRelationType, LOOK_RELATIONS } from './constants';
 import { LookFilter } from './dtos';
@@ -19,7 +24,8 @@ export class LooksResolver extends BaseResolver<LookRelationType> {
 
   constructor(
     private readonly looksService: LooksService,
-    private readonly lookSearchService: LookSearchService
+    private readonly lookSearchService: LookSearchService,
+    private readonly likesService: LikesService
   ) {
     super();
   }
@@ -44,6 +50,26 @@ export class LooksResolver extends BaseResolver<LookRelationType> {
     @Info() info?: GraphQLResolveInfo
   ): Promise<Look[]> {
     const ids = await this.lookSearchService.search(query, pageInput);
+
+    return await this.looksService.list(
+      { idIn: ids },
+      null,
+      this.getRelationsFromInfo(info)
+    );
+  }
+
+  @Query(() => [Look])
+  @UseGuards(JwtVerifyGuard)
+  async likingLooks(
+    @CurrentUser() { sub: userId }: JwtPayload,
+    @Args('pageInput') pageInput: PageInput,
+    @Info() info?: GraphQLResolveInfo
+  ): Promise<Look[]> {
+    const ids = await this.likesService.findOwnerIds(
+      userId,
+      LikeOwnerType.Look,
+      pageInput
+    );
 
     return await this.looksService.list(
       { idIn: ids },
