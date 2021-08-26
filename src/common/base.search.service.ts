ce0ebf1de16db996142e720b480024076f69d109
greatSumini <greatSumini@gmail.com>
2021-08-26 19:34:06 +0900

    feat(common): :sparkles: add BaseSearchService
    - ~SearchService들은 이것을 extend하면 된다. search는 이미 구현되어 있음

---
+++ src/common/base.search.service.ts
@@ -0,0 +1,36 @@
+import { SearchService } from '@providers/elasticsearch/provider.service';
+
+import { PageInput } from './dtos';
+
+export abstract class BaseSearchService<
+  SearchBody extends {
+    id: number;
+  }
+> {
+  abstract indexName: string;
+  abstract queryFields: Array<keyof SearchBody>;
+
+  abstract searchService: SearchService;
+
+  abstract toBody(data: { id: number }): SearchBody;
+
+  abstract index(id: number): Promise<void>;
+
+  async search(query: string, pageInput: PageInput): Promise<number[]> {
+    const sources = await this.searchService.search<SearchBody>(
+      this.indexName,
+      query,
+      this.queryFields,
+      {
+        from: pageInput?.offset ?? 0,
+        size: pageInput?.limit ?? 20,
+      }
+    );
+
+    return sources.map((source) => source.id);
+  }
+
+  abstract update(id: number): Promise<void>;
+
+  abstract remove(id: number): Promise<void>;
+}
