0881fc18ffa138d96f2584e12cbb7c866c918820
Yang <ybh1760@gmail.com>
2021-08-26 16:29:05 +0900

    enhance(application): :label: Wrap PartialType to CreateOrderVbankReceiptInput dto

--- src/modules/order/orders/dtos/create-vbank-receipt-input.ts
+++ src/modules/order/orders/dtos/create-vbank-receipt-input.ts
@@ -1,10 +1,13 @@
-import { InputType, PickType } from '@nestjs/graphql';
+import { InputType, PartialType, PickType } from '@nestjs/graphql';
 
 import { OrderVbankReceipt } from '../models';
 
+// @FIXME: graphQL Arg nullbale true 옵션을 적용하였는데도, classValidator를 수행하는 문제가 있어, 임시방편으로 partialType을 하였으나 추후에 고쳐야합니다.
 @InputType()
-export class CreateOrderVbankReceiptInput extends PickType(
-  OrderVbankReceipt,
-  ['bankCode', 'number', 'ownerName', 'due'],
-  InputType
+export class CreateOrderVbankReceiptInput extends PartialType(
+  PickType(
+    OrderVbankReceipt,
+    ['bankCode', 'number', 'ownerName', 'due'],
+    InputType
+  )
 ) {}
