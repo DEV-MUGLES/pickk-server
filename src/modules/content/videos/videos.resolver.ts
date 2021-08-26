3585f5484eb8d9da8c47b9147f6656e7a4fd6141
greatSumini <greatSumini@gmail.com>
2021-08-27 03:24:01 +0900

    feat(application): :sparkles: add likingVideos query

--- src/modules/content/videos/videos.resolver.ts
+++ src/modules/content/videos/videos.resolver.ts
@@ -1,11 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UseGuards } from '@nestjs/common';
 import { Args, Info, Mutation, Query } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
 
+import { CurrentUser } from '@auth/decorators';
+import { JwtVerifyGuard } from '@auth/guards';
+import { JwtPayload } from '@auth/models';
 import { PageInput } from '@common/dtos';
 import { BaseResolver } from '@common/base.resolver';
 
 import { VideoSearchService } from '@mcommon/search/video.search.service';
+import { LikeOwnerType } from '@content/likes/constants';
+import { LikesService } from '@content/likes/likes.service';
 
 import { VideoRelationType, VIDEO_RELATIONS } from './constants';
 import { VideoFilter } from './dtos';
@@ -19,7 +24,8 @@ export class VideosResolver extends BaseResolver<VideoRelationType> {
 
   constructor(
     private readonly videosService: VideosService,
-    private readonly videosSearchService: VideoSearchService
+    private readonly videosSearchService: VideoSearchService,
+    private readonly likesService: LikesService
   ) {
     super();
   }
@@ -56,5 +62,25 @@ export class VideosResolver extends BaseResolver<VideoRelationType> {
   async indexVideo() {
     await this.videosSearchService.index(1);
     await this.videosSearchService.index(2);
+  }
+
+  @Query(() => [Video])
+  @UseGuards(JwtVerifyGuard)
+  async likingVideos(
+    @CurrentUser() { sub: userId }: JwtPayload,
+    @Args('pageInput') pageInput: PageInput,
+    @Info() info?: GraphQLResolveInfo
+  ): Promise<Video[]> {
+    const ids = await this.likesService.findOwnerIds(
+      userId,
+      LikeOwnerType.Video,
+      pageInput
+    );
+
+    return await this.videosService.list(
+      { idIn: ids },
+      null,
+      this.getRelationsFromInfo(info)
+    );
   }
 }
