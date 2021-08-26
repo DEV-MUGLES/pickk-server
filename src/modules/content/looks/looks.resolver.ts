1192d3e8c43b263567cb571f0a0d708fe6bcb8d0
greatSumini <greatSumini@gmail.com>
2021-08-27 03:13:20 +0900

    feat(application): :sparkles: add searchLook query

--- src/modules/content/looks/looks.resolver.ts
+++ src/modules/content/looks/looks.resolver.ts
@@ -1,9 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { Args, Info, Query } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
 
 import { PageInput } from '@common/dtos';
 import { BaseResolver } from '@common/base.resolver';
+
+import { LookSearchService } from '@mcommon/search/look.search.service';
 
 import { LookRelationType, LOOK_RELATIONS } from './constants';
 import { LookFilter } from './dtos';
@@ -16,7 +18,8 @@ export class LooksResolver extends BaseResolver<LookRelationType> {
   relations = LOOK_RELATIONS;
 
   constructor(
-    @Inject(LooksService) private readonly looksService: LooksService
+    private readonly looksService: LooksService,
+    private readonly lookSearchService: LookSearchService
   ) {
     super();
   }
@@ -30,6 +33,21 @@ export class LooksResolver extends BaseResolver<LookRelationType> {
     return await this.looksService.list(
       filter,
       pageInput,
+      this.getRelationsFromInfo(info)
+    );
+  }
+
+  @Query(() => [Look])
+  async searchLook(
+    @Args('query') query: string,
+    @Args('pageInput', { nullable: true }) pageInput?: PageInput,
+    @Info() info?: GraphQLResolveInfo
+  ): Promise<Look[]> {
+    const ids = await this.lookSearchService.search(query, pageInput);
+
+    return await this.looksService.list(
+      { idIn: ids },
+      null,
       this.getRelationsFromInfo(info)
     );
   }
