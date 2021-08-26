6f7ca0a986eef0b392a0c0dac3a0b74a9d5708cd
greatSumini <greatSumini@gmail.com>
2021-08-26 19:37:05 +0900

    feat(application): :sparkles: add idIn to Keyword,Digest,Look,VideoFilter

--- src/modules/content/looks/dtos/look.filter.ts
+++ src/modules/content/looks/dtos/look.filter.ts
@@ -10,18 +10,20 @@ export class LookUserFilter {
 
 @InputType()
 export class LookFilter implements Partial<Omit<ILook, 'user'>> {
-  excludeFields: Array<keyof LookFilter> = ['styleTagIdIn'];
+  excludeFields?: Array<keyof LookFilter> = ['styleTagIdIn'];
 
+  @Field(() => [Int], { nullable: true })
+  idIn?: number[];
   @Field(() => Int, { nullable: true })
-  userId: number;
+  userId?: number;
   @Field(() => [Int], { nullable: true })
-  userIdIn: number[];
+  userIdIn?: number[];
   @Field(() => LookUserFilter, { nullable: true })
-  user: LookUserFilter;
+  user?: LookUserFilter;
 
   @Field(() => [Int], { nullable: true })
-  styleTagIdIn: number[];
+  styleTagIdIn?: number[];
 
   @Field(() => String, { defaultValue: 'id' })
-  orderBy: keyof ILook;
+  orderBy?: keyof ILook;
 }
