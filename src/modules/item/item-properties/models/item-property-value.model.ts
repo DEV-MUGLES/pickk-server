a90ffe95887dff38693077830dd1ad0f043db085
greatSumini <greatSumini@gmail.com>
2021-08-29 06:16:19 +0900

    feat(domain): :sparkles: set ItemPropertyValue as gql field

--- src/modules/item/item-properties/models/item-property-value.model.ts
+++ src/modules/item/item-properties/models/item-property-value.model.ts
@@ -1,6 +1,10 @@
-import { ObjectType } from '@nestjs/graphql';
+import { Field, ObjectType } from '@nestjs/graphql';
 
 import { ItemPropertyValueEntity } from '../entities';
+import { ItemProperty } from './item-property.model';
 
 @ObjectType()
-export class ItemPropertyValue extends ItemPropertyValueEntity {}
+export class ItemPropertyValue extends ItemPropertyValueEntity {
+  @Field(() => ItemProperty)
+  property: ItemProperty;
+}
