b43eda791a86f680f486234dcd056976bc9a35c9
Yang <ybh1760@gmail.com>
2021-08-29 19:03:44 +0900

    feat(job): :sparkles: add  ConfirmOrderItemsStep

---
+++ src/batch/jobs/order/confirm-order-items/steps/confirm-order-items.step.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import dayjs from 'dayjs';
+import { IsNull, LessThan } from 'typeorm';
+
+import { BaseStep } from '@batch/jobs/base.step';
+import { OrderItemStatus } from '@order/order-items/constants';
+import { OrderItemsRepository } from '@order/order-items/order-items.repository';
+
+@Injectable()
+export class ConfirmOrderItemsStep extends BaseStep {
+  constructor(
+    @InjectRepository(OrderItemsRepository)
+    private readonly orderItemsRepository: OrderItemsRepository
+  ) {
+    super();
+  }
+  async tasklet() {
+    const notConfirmedOrderItemMerchantUids = (
+      await this.orderItemsRepository.find({
+        select: ['merchantUid'],
+        where: {
+          status: OrderItemStatus.Shipped,
+          isConfirmed: false,
+          claimStatus: IsNull(),
+          shippedAt: LessThan(dayjs().add(-14, 'day').toDate()),
+        },
+      })
+    ).map(({ merchantUid }) => merchantUid);
+
+    await this.orderItemsRepository.update(notConfirmedOrderItemMerchantUids, {
+      isConfirmed: true,
+    });
+  }
+}
