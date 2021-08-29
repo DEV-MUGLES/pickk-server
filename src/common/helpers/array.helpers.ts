9cd1a8642878e16216602b0addb7f93698ae2f97
greatSumini <greatSumini@gmail.com>
2021-08-30 04:42:59 +0900

    feat(common): :sparkles: add diffArr helper

--- src/common/helpers/array.helpers.ts
+++ src/common/helpers/array.helpers.ts
@@ -1,2 +1,8 @@
 export const isAllEleSame = (arr: unknown[]): boolean =>
   [...new Set(arr)].length <= 1;
+
+/** 두 배열의 차집합을 반환합니다.
+ * @param origin 원본 배열입니다.
+ * @param sub 뺄 배열입니다. */
+export const diffArr = <T = unknown>(origin: T[], sub: T[]): T[] =>
+  origin.filter((v) => !sub.includes(v));
