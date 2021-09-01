ae758c5ebff1a221e12f70e492bf5618beafff3e
Yang <ybh1760@gmail.com>
2021-09-01 19:11:51 +0900

    chore(job): add hitScoreVariable datas

---
+++ src/batch/jobs/content/constants/hit-score-variable.constants.ts
@@ -0,0 +1,23 @@
+export const digestHitScoreVariable = {
+  primaryCliffHour: 360,
+  secondaryCliffHour: 960,
+  n1: 1,
+  n2: 1,
+  n3: 2,
+};
+
+export const lookHitScoreVariable = {
+  primaryCliffHour: 288,
+  secondaryCliffHour: 1008,
+  n1: 0.5,
+  n2: 1,
+  n3: 2,
+};
+
+export const videoHitScoreVariable = {
+  primaryCliffHour: 288,
+  secondaryCliffHour: 1008,
+  n1: 1,
+  n2: 1.5,
+  n3: 2,
+};
