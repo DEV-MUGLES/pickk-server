c19207d0c96742bed1ec9869b3f1b720cdd412c5
Yang <ybh1760@gmail.com>
2021-09-01 14:00:07 +0900

    enhance(provider): :art: change [] to Array<> in CrawlItemOptionResponseDto

--- src/providers/crawler/dtos/crawl-item-option.dto.ts
+++ src/providers/crawler/dtos/crawl-item-option.dto.ts
@@ -1,7 +1,7 @@
 export class CrawlItemOptionResponseDto {
-  values: { [name: string]: string[] };
-  isSoldout: number[][];
-  productPriceVariants: { option: number[]; price: number }[];
-  optionPriceVariants: { option: number[]; price: number }[];
+  values: { [name: string]: Array<string> };
+  isSoldout: Array<Array<number>>;
+  productPriceVariants: Array<{ option: Array<number>; price: number }>;
+  optionPriceVariants: Array<{ option: Array<number>; price: number }>;
   url: string;
 }
