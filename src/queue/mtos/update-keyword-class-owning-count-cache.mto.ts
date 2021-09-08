725db76383cb11fe68dee1aca844b5e3845e4bb5
Yang <ybh1760@gmail.com>
2021-09-08 12:44:36 +0900

    refactor(queue): :art: refactor UpdateKeywordClassOwningCountCacheConsumer and OwningCountCacheProducer

--- src/queue/mtos/update-keyword-class-owning-count-cache.mto.ts
+++ src/queue/mtos/update-keyword-class-owning-count-cache.mto.ts
@@ -1,10 +1,4 @@
-export enum UpdateKeywordClassOwningCountCacheType {
-  Increase = 'increase',
-  Decrease = 'decrease',
-}
-
 export class UpdateKeywordClassOwningCountCacheMto {
   userId: number;
   keywordId: number;
-  type: UpdateKeywordClassOwningCountCacheType;
 }
