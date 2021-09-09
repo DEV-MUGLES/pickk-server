3ee189ca2b84b7b66b63e0ada232cc8575982446
Yang <ybh1760@gmail.com>
2021-09-09 17:12:27 +0900

    enhance(application): :art: add processVbankPaid to Order model and apply to ProcessVbankPaidOrderConsumer

--- src/modules/order/orders/consumers/process-vbank-paid-order.consumer.ts
+++ src/modules/order/orders/consumers/process-vbank-paid-order.consumer.ts
@@ -1,12 +1,9 @@
 import { InjectRepository } from '@nestjs/typeorm';
 import { SqsMessageHandler, SqsProcess } from '@pickk/nestjs-sqs';
 
-import { OrderItemStatus } from '@order/order-items/constants';
-
 import { PROCESS_VBANK_PAID_ORDER_QUEUE } from '@queue/constants';
 import { ProcessVbankPaidOrderMto } from '@queue/mtos';
 
-import { OrderStatus } from '../constants';
 import { OrdersRepository } from '../orders.repository';
 
 @SqsProcess(PROCESS_VBANK_PAID_ORDER_QUEUE)
@@ -20,13 +17,7 @@ export class ProcessVbankPaidOrderConsumer {
   async processVbankPaid(message: AWS.SQS.Message) {
     const { merchantUid }: ProcessVbankPaidOrderMto = JSON.parse(message.Body);
     const order = await this.ordersRepository.get(merchantUid, ['orderItems']);
-    order.status = OrderStatus.Paid;
-    order.paidAt = new Date();
-
-    for (const orderItem of order.orderItems) {
-      orderItem.status = OrderItemStatus.Paid;
-      orderItem.paidAt = new Date();
-    }
+    order.processVbankPaid();
     await this.ordersRepository.save(order);
   }
 }
