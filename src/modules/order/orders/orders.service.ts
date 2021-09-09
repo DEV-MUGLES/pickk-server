e58a179378d6507b3f5591247bf7b6d68cb34873
Yang <ybh1760@gmail.com>
2021-09-09 20:36:29 +0900

    feat(queue): :sparkles: add sendVbankNotiAlimtalk and sendOrderCompletedAlimtalk to OrdersProducer

--- src/modules/order/orders/orders.service.ts
+++ src/modules/order/orders/orders.service.ts
@@ -34,6 +34,7 @@ import {
 } from './dtos';
 import { OrderFactory } from './factories';
 import { Order } from './models';
+import { OrdersProducer } from './producers';
 
 import { OrdersRepository } from './orders.repository';
 
@@ -48,7 +49,8 @@ export class OrdersService {
     private readonly couponsService: CouponsService,
     private readonly productsService: ProductsService,
     private readonly paymentsService: PaymentsService,
-    private readonly usersService: UsersService
+    private readonly usersService: UsersService,
+    private readonly ordersProducer: OrdersProducer
   ) {}
 
   async checkBelongsTo(merchantUid: string, userId: number): Promise<void> {
@@ -156,7 +158,9 @@ export class OrdersService {
     }
 
     order.complete(createOrderVbankReceiptInput);
-    return await this.ordersRepository.save(order);
+    const completedOrder = await this.ordersRepository.save(order);
+    await this.sendCompleteAlimtalk(order);
+    return completedOrder;
   }
 
   async dodgeVbank(merchantUid: string): Promise<Order> {
@@ -193,5 +197,13 @@ export class OrdersService {
     order.requestRefund(input);
 
     return await this.ordersRepository.save(order);
+  }
+
+  private async sendCompleteAlimtalk(order: Order) {
+    if (order.vbankInfo) {
+      await this.ordersProducer.sendVbankNotiAlimtalk(order);
+    } else {
+      await this.ordersProducer.sendOrderCompletedAlimtalk(order);
+    }
   }
 }
--- src/modules/order/orders/producers/orders.producer.ts
+++ src/modules/order/orders/producers/orders.producer.ts
@@ -4,10 +4,14 @@ import { SqsService } from '@pickk/nestjs-sqs';
 import {
   RESTORE_DEDUCTED_PRODUCT_STOCK_QUEUE,
   SEND_CANCEL_ORDER_APPROVED_ALIMTALK_QUEUE,
+  SEND_ORDER_COMPLETED_ALIMTALK_QUEUE,
+  SEND_VBANK_NOTI_ALIMTALK_QUEUE,
 } from '@queue/constants';
 import {
   RestoreDeductedProductStockMto,
   SendCancelOrderApprovedAlimtalkMto,
+  SendOrderCompletedAlimtalkMto,
+  SendVbankNotiAlimtalkMto,
 } from '@queue/mtos';
 
 import { Order } from '../models';
@@ -32,6 +36,26 @@ export class OrdersProducer {
       {
         id: canceledOrder.merchantUid,
         body: { canceledOrder },
+      }
+    );
+  }
+
+  async sendVbankNotiAlimtalk(order: Order) {
+    await this.sqsService.send<SendVbankNotiAlimtalkMto>(
+      SEND_VBANK_NOTI_ALIMTALK_QUEUE,
+      {
+        id: order.merchantUid,
+        body: { order },
+      }
+    );
+  }
+
+  async sendOrderCompletedAlimtalk(order: Order) {
+    await this.sqsService.send<SendOrderCompletedAlimtalkMto>(
+      SEND_ORDER_COMPLETED_ALIMTALK_QUEUE,
+      {
+        id: order.merchantUid,
+        body: { order },
       }
     );
   }
