88a71e49f5e9456c08d10565f5a4fbb0e50b4999
Yang <ybh1760@gmail.com>
2021-09-09 18:58:59 +0900

    feat(templates): :sparkles: add CompleteOrderTemplate

---
+++ src/templates/alimtalk/customers/complete-order.template.ts
@@ -0,0 +1,35 @@
+import { AlimtalkMessageRequest } from 'nest-sens';
+
+import { Order } from '@order/orders/models';
+
+import { addCommas } from '@common/helpers';
+
+import { getPurchaseItemInfo } from '../../helpers';
+
+export class CompleteOrderTemplate {
+  static code = 'ccomp01';
+
+  static toRequest(order: Order): Omit<AlimtalkMessageRequest, 'plusFriendId'> {
+    return {
+      templateCode: this.code,
+      messages: [
+        {
+          to: order.buyer.phoneNumber,
+          content: this.toContent(order),
+        },
+      ],
+    };
+  }
+
+  static toContent(order: Order) {
+    const { buyer, merchantUid, totalPayAmount, receiver, orderItems } = order;
+    return `${buyer.name}님의 주문이 완료되었습니다.
+  
+  ▶ 주문번호 : ${merchantUid}
+  ▶ 상품명 : ${getPurchaseItemInfo(orderItems)}
+  ▶ 결제금액 : ${addCommas(totalPayAmount)}원
+  ▶ 배송지 : [${receiver.postalCode}] ${receiver.baseAddress} ${
+      receiver.detailAddress
+    }`;
+  }
+}
--- src/templates/alimtalk/customers/index.ts
+++ src/templates/alimtalk/customers/index.ts
@@ -1,2 +1,3 @@
 export * from './refund-requested.template';
 export * from './cancel-order-approved.template';
+export * from './complete-order.template';
