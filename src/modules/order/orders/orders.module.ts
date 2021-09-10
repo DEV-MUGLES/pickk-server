96cdca3b03a2e6c7efe88d684210cdf96c71cda8
Yang <ybh1760@gmail.com>
2021-09-10 17:42:54 +0900

    feat(queue): :sparkles: add SendRefundRequestedAlimtalkConsumer

--- src/modules/order/orders/orders.module.ts
+++ src/modules/order/orders/orders.module.ts
@@ -7,6 +7,7 @@ import {
   RESTORE_DEDUCTED_PRODUCT_STOCK_QUEUE,
   SEND_CANCEL_ORDER_APPROVED_ALIMTALK_QUEUE,
   SEND_ORDER_COMPLETED_ALIMTALK_QUEUE,
+  SEND_REFUND_REQUESTED_ALIMTALK_QUEUE,
   SEND_VBANK_PAID_ALIMTALK_QUEUE,
 } from '@queue/constants';
 
@@ -16,6 +17,7 @@ import { CouponsModule } from '@order/coupons/coupons.module';
 import { PointsModule } from '@order/points/points.module';
 import { PaymentsModule } from '@payment/payments/payments.module';
 import { UsersModule } from '@user/users/users.module';
+import { RefundRequestsModule } from '@order/refund-requests/refund-requests.module';
 
 import { OrdersConsumers } from './consumers';
 import { OrdersProducer } from './producers';
@@ -35,6 +37,7 @@ import { OrdersService } from './orders.service';
     PointsModule,
     PaymentsModule,
     UsersModule,
+    RefundRequestsModule,
     SqsModule.registerQueue(
       {
         name: RESTORE_DEDUCTED_PRODUCT_STOCK_QUEUE,
@@ -53,6 +56,9 @@ import { OrdersService } from './orders.service';
       },
       {
         name: SEND_ORDER_COMPLETED_ALIMTALK_QUEUE,
+      },
+      {
+        name: SEND_REFUND_REQUESTED_ALIMTALK_QUEUE,
       }
     ),
   ],
