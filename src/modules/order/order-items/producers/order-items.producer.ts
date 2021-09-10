43c996f108abae3f73eec90c80ca77eb0462e67b
Yang <ybh1760@gmail.com>
2021-09-10 18:42:41 +0900

    enhance(queue): :art: add sendExchangeRequestedAlimtalk mto type in OrderItemsProducer

--- src/modules/order/order-items/producers/order-items.producer.ts
+++ src/modules/order/order-items/producers/order-items.producer.ts
@@ -2,21 +2,25 @@ import { Injectable } from '@nestjs/common';
 import { SqsService } from '@pickk/nestjs-sqs';
 
 import { SEND_EXCHANGE_REQUESTED_ALIMTALK_QUEUE } from '@queue/constants';
+import { SendExchangeRequestedAlimtalkMto } from '@queue/mtos';
 
 @Injectable()
 export class OrderItemsProducer {
   constructor(private readonly sqsService: SqsService) {}
 
   async sendExchangeRequestedAlimtalk(
-    merchantUid: string,
+    orderMerchantUid: string,
     exchangeRequestId: number
   ) {
-    await this.sqsService.send(SEND_EXCHANGE_REQUESTED_ALIMTALK_QUEUE, {
-      id: merchantUid,
-      body: {
-        merchantUid,
-        exchangeRequestId,
-      },
-    });
+    await this.sqsService.send<SendExchangeRequestedAlimtalkMto>(
+      SEND_EXCHANGE_REQUESTED_ALIMTALK_QUEUE,
+      {
+        id: orderMerchantUid,
+        body: {
+          orderMerchantUid,
+          exchangeRequestId,
+        },
+      }
+    );
   }
 }
