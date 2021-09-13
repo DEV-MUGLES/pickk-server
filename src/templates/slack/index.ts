c0adc223c1c43172f036ca60a1613eb32065959c
Yang <ybh1760@gmail.com>
2021-09-13 16:52:50 +0900

    feat(templates): :sparkles: add slack InquiryCreatedTemplate

--- src/templates/slack/index.ts
+++ src/templates/slack/index.ts
@@ -1,3 +1,4 @@
 export * from './item-creation-fail.template';
 export * from './item-creation-success.template';
 export * from './item-report.template';
+export * from './inquiry-creation.template';
