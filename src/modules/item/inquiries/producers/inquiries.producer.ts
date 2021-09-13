ee14fc77380dbab3c219e428b504891e6a962fde
Yang <ybh1760@gmail.com>
2021-09-13 16:54:13 +0900

    feat(queue): :sparkles: add InquiriesProducer

---
+++ src/modules/item/inquiries/producers/inquiries.producer.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@nestjs/common';
+import { SqsService } from '@pickk/nestjs-sqs';
+
+import { SEND_INQUIRY_CREATED_SLACK_MESSAGE_QUEUE } from '@queue/constants';
+import { SendInquiryCreatedSlackMessageMto } from '@queue/mtos';
+
+import { Inquiry } from '../models';
+
+@Injectable()
+export class InquiriesProducer {
+  constructor(private readonly sqsService: SqsService) {}
+
+  async sendInquiryCreationSlackMessage(inquiry: Inquiry) {
+    await this.sqsService.send<SendInquiryCreatedSlackMessageMto>(
+      SEND_INQUIRY_CREATED_SLACK_MESSAGE_QUEUE,
+      {
+        id: inquiry.id.toString(),
+        body: {
+          inquiry,
+        },
+      }
+    );
+  }
+}
