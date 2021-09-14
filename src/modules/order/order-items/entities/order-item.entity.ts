9b96939da930862b5ddc48e1f36ba22d7631a26f
greatSumini <greatSumini@gmail.com>
2021-09-15 06:19:37 +0900

    feat(infra): :card_file_box: update OrderItem fields
    - add shippingFee,isFreeShippingPackage
    - remove withdrawnAt

--- src/modules/order/order-items/entities/order-item.entity.ts
+++ src/modules/order/order-items/entities/order-item.entity.ts
@@ -70,8 +70,10 @@ export class OrderItemEntity implements IOrderItem {
     this.isSettled = attributes.isSettled;
     this.isDelaying = attributes.isDelaying;
     this.isProcessDelaying = attributes.isProcessDelaying;
+    this.isFreeShippingPackage = attributes.isFreeShippingPackage;
 
     this.itemFinalPrice = attributes.itemFinalPrice;
+    this.shippingFee = attributes.shippingFee;
     this.couponDiscountAmount = attributes.couponDiscountAmount;
     this.usedPointAmount = attributes.usedPointAmount;
 
@@ -88,7 +90,6 @@ export class OrderItemEntity implements IOrderItem {
     this.vbankReadyAt = attributes.vbankReadyAt;
     this.vbankDodgedAt = attributes.vbankDodgedAt;
     this.paidAt = attributes.paidAt;
-    this.withdrawnAt = attributes.withdrawnAt;
     this.shipReadyAt = attributes.shipReadyAt;
     this.shippingAt = attributes.shippingAt;
     this.shippedAt = attributes.shippedAt;
@@ -112,18 +113,13 @@ export class OrderItemEntity implements IOrderItem {
     this.settledAt = attributes.settledAt;
   }
 
-  @Field(() => String, {
-    description:
-      '주문상품고유번호. PrimaryColumn입니다. order의 merchantUid + 숫자 1자리 형식입니다.',
-  })
+  @Field({ description: 'order.merchantUid + 숫자 1자리' })
   @PrimaryColumn({ type: 'char', length: 22 })
   @IsString()
   merchantUid: string;
-
   @Field()
   @CreateDateColumn()
   createdAt: Date;
-
   @Field()
   @UpdateDateColumn()
   updatedAt: Date;
@@ -131,99 +127,54 @@ export class OrderItemEntity implements IOrderItem {
   @Field(() => User, { nullable: true })
   @ManyToOne('UserEntity', { nullable: true })
   user?: User;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   userId?: number;
-
   @Field(() => Seller, { nullable: true })
   @ManyToOne('SellerEntity', { nullable: true })
   seller?: Seller;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   sellerId?: number;
-
   @Field(() => Item, { nullable: true })
   @ManyToOne('ItemEntity', { nullable: true })
   item?: Item;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   itemId?: number;
-
   @Field(() => Product, { nullable: true })
   @ManyToOne('ProductEntity', { nullable: true })
   product?: Product;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   productId: number;
 
   @Field(() => Coupon, { nullable: true })
   @ManyToOne('CouponEntity', { nullable: true })
   usedCoupon?: Coupon;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   usedCouponId?: number;
-
   @Field(() => Shipment, { nullable: true })
   @OneToOne('ShipmentEntity', { nullable: true, cascade: true })
   @JoinColumn()
   shipment: Shipment;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   shipmentId: number;
 
   @ManyToOne('OrderEntity')
   order: IOrder;
-
   @Field()
   @Column({ type: 'char', length: 20 })
   orderMerchantUid: string;
 
   @ManyToOne('RefundRequestEntity', 'orderItems')
   refundRequest: IRefundRequest;
-
   @OneToOne('ExchangeRequestEntity', 'orderItem', { cascade: true })
   exchangeRequest: IExchangeRequest;
 
-  @Field(() => String, {
-    description:
-      '프론트엔드에서 보여주기 위한 status/claimStatus 표시값입니다.',
-  })
+  @Field({ description: '프론트엔드를 위한 status/claimStatus 표시값입니다.' })
   get statusDisplayName(): string {
     return (
       getOrderItemClaimStatusDisplayName(this.claimStatus) ??
@@ -232,13 +183,9 @@ export class OrderItemEntity implements IOrderItem {
   }
 
   @Field(() => OrderItemStatus)
-  @Column({
-    type: 'enum',
-    enum: OrderItemStatus,
-  })
+  @Column({ type: 'enum', enum: OrderItemStatus })
   @IsEnum(OrderItemStatus)
   status: OrderItemStatus;
-
   @Field(() => OrderItemClaimStatus, { nullable: true })
   @Column({
     type: 'enum',
@@ -248,7 +195,6 @@ export class OrderItemEntity implements IOrderItem {
   @IsEnum(OrderItemClaimStatus)
   @IsOptional()
   claimStatus?: OrderItemClaimStatus;
-
   @Field(() => Int)
   @Column({ type: 'smallint', unsigned: true })
   @Min(1)
@@ -258,87 +204,67 @@ export class OrderItemEntity implements IOrderItem {
   @Column({ default: false })
   @IsOptional()
   isConfirmed: boolean;
-
   @Field({ defaultValue: false })
   @Column({ default: false })
   @IsOptional()
   isShipReserved: boolean;
-
   @Field({ defaultValue: false })
   @Column({ default: false })
   @IsOptional()
   isSettled: boolean;
-
   @Field({ defaultValue: false })
   @Column({ default: false })
   @IsOptional()
   isDelaying: boolean;
-
   @Field({ defaultValue: false })
   @Column({ default: false })
   @IsOptional()
   isProcessDelaying: boolean;
+  @Field()
+  @Column()
+  isFreeShippingPackage: boolean;
 
   @Field(() => Int)
   @Column({ unsigned: true })
   @Min(1)
   itemFinalPrice: number;
-
+  @Field(() => Int)
+  @Column({ unsigned: true })
+  @Min(1)
+  shippingFee: number;
   @Field(() => Int)
   @Column({ unsigned: true, default: 0 })
   @Min(0)
   couponDiscountAmount: number;
-
   @Field(() => Int)
   @Column({ unsigned: true, default: 0 })
   @Min(0)
   usedPointAmount: number;
 
   @Field({ nullable: true })
-  @Column({
-    type: 'varchar',
-    length: 30,
-    nullable: true,
-  })
+  @Column({ length: 30, nullable: true })
   @MaxLength(30)
   @IsOptional()
   usedCouponName?: string;
 
   @Field()
-  @Column({
-    type: 'varchar',
-    length: 30,
-  })
+  @Column({ length: 30 })
   @MaxLength(30)
   brandNameKor: string;
-
   @Field()
   @Column()
   @IsString()
   itemName: string;
-
   @Field()
   @Column()
   @IsString()
   productVariantName: string;
 
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   recommenderId?: number;
-
-  @Field({
-    nullable: true,
-  })
-  @Column({
-    type: 'varchar',
-    nullable: true,
-    length: 11,
-  })
+  @Field({ nullable: true })
+  @Column({ nullable: true, length: 11 })
   recommenderNickname?: string;
 
   @Field(() => ContentType, { nullable: true })
@@ -348,44 +274,28 @@ export class OrderItemEntity implements IOrderItem {
     nullable: true,
   })
   recommendContentType?: ContentType;
-
-  @Field(() => Int, {
-    nullable: true,
-  })
-  @Column({
-    type: 'int',
-    nullable: true,
-  })
+  @Field(() => Int, { nullable: true })
+  @Column({ type: 'int', nullable: true })
   recommendContentItemId?: number;
 
   @Field({ nullable: true })
   @Column({ nullable: true })
   failedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   vbankReadyAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   vbankDodgedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   paidAt?: Date;
-
-  @Field({ nullable: true })
-  @Column({ nullable: true })
-  withdrawnAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   shipReadyAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   shippingAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   shippedAt?: Date;
@@ -393,23 +303,18 @@ export class OrderItemEntity implements IOrderItem {
   @Field({ nullable: true })
   @Column({ nullable: true })
   cancelRequestedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   cancelledAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   exchangeRequestedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   exchangedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   refundRequestedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   refundedAt?: Date;
@@ -417,11 +322,9 @@ export class OrderItemEntity implements IOrderItem {
   @Field({ description: '지연발송 전환 시점', nullable: true })
   @Column({ nullable: true })
   delayedAt: Date;
-
   @Field({ description: '지연발송 예정일', nullable: true })
   @Column({ nullable: true })
   delayedShipExpectedAt: Date;
-
   @Field({ description: '처리지연 전환 시점', nullable: true })
   @Column({ nullable: true })
   processDelayedAt: Date;
@@ -429,11 +332,9 @@ export class OrderItemEntity implements IOrderItem {
   @Field({ description: '예약발송 예정일', nullable: true })
   @Column({ nullable: true })
   shipReservedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   confirmedAt?: Date;
-
   @Field({ nullable: true })
   @Column({ nullable: true })
   settledAt?: Date;
