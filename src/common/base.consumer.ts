78e8de1cadfcc390646525ef81fc055731c1bd01
Yang <ybh1760@gmail.com>
2021-09-14 17:50:40 +0900

    feat(queue): :sparkles: add BaseConsumer abstract class for log errors

---
+++ src/common/base.consumer.ts
@@ -0,0 +1,16 @@
+import { Logger } from '@nestjs/common';
+import { SqsConsumerEvent, SqsConsumerEventHandler } from '@pickk/nestjs-sqs';
+
+export abstract class BaseConsumer {
+  abstract logger: Logger;
+
+  @SqsConsumerEventHandler(SqsConsumerEvent.PROCESSING_ERROR)
+  processingError(error: Error) {
+    this.logger.error(error);
+  }
+
+  @SqsConsumerEventHandler(SqsConsumerEvent.ERROR)
+  error(error: Error) {
+    this.logger.error(error);
+  }
+}
