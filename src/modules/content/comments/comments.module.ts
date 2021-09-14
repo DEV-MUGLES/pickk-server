9bfefa8aa072c8d4c5b2a7df9438696a998b2a6e
양병훈 <ybh1760@gmail.com>
2021-09-15 02:57:52 +0900

    Fix/211/fix all settled resolve value (#217)
    * feat(queue): :sparkles: add BaseConsumer abstract class for log errors
    
    * enhance(queue): :art: change allSettled to Promise.all and apply BaseConsumer
    
    * feat(queue): :sparkles: add dead letter queue to localstack and rename O2O_SEND_CANCEL_ORDER_APPROVED_ALIMTALK_QUEUE to O2O-SEND_CANCEL_ORDER_APPROVED_ALIMTALK
    
    * enhance(queue): :fire: remove MessageRetentionPeriod queue attributes in scripts/init-localstack.sh
    
    * enhance(queue): :art: fix digestAverageRating issue in UpdateItemDigestStatisticsConsumer
    
    digestAverageRating 계산시 digest의 rating의 평균을 2로 나누도록 변경하였습니다.
    
    * fix(queue): :bug: fix localstack sqs attributes error

--- src/modules/content/comments/comments.module.ts
+++ src/modules/content/comments/comments.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { SqsModule, SqsQueueType } from '@pickk/nestjs-sqs';
 
@@ -42,6 +42,7 @@ import { CommentsService } from './comments.service';
     LikesModule,
   ],
   providers: [
+    Logger,
     CommentsResolver,
     CommentsService,
     UpdateCommentLikeCountConsumer,
--- src/modules/content/keywords/consumers/update-keyword-like-count.consumer.ts
+++ src/modules/content/keywords/consumers/update-keyword-like-count.consumer.ts
@@ -1,21 +1,26 @@
+import { Logger } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { SqsMessageHandler, SqsProcess } from '@pickk/nestjs-sqs';
 
-import { allSettled } from '@common/helpers';
-import { LikeOwnerType } from '@content/likes/constants';
-import { LikesService } from '@content/likes/likes.service';
+import { BaseConsumer } from '@common/base.consumer';
 import { UPDATE_KEYWORD_LIKE_COUNT_QUEUE } from '@queue/constants';
 import { UpdateLikeCountMto } from '@queue/mtos';
+
+import { LikeOwnerType } from '@content/likes/constants';
+import { LikesService } from '@content/likes/likes.service';
 
 import { KeywordsRepository } from '../keywords.repository';
 
 @SqsProcess(UPDATE_KEYWORD_LIKE_COUNT_QUEUE)
-export class UpdateKeywordLikeCountConsumer {
+export class UpdateKeywordLikeCountConsumer extends BaseConsumer {
   constructor(
     @InjectRepository(KeywordsRepository)
     private readonly keywordsRepository: KeywordsRepository,
-    private readonly likesService: LikesService
-  ) {}
+    private readonly likesService: LikesService,
+    readonly logger: Logger
+  ) {
+    super();
+  }
 
   @SqsMessageHandler(true)
   async updateLikeCount(messages: AWS.SQS.Message[]) {
@@ -24,7 +29,7 @@ export class UpdateKeywordLikeCountConsumer {
     );
 
     const uniqueIds = [...new Set(mtos.map(({ id }) => id))];
-    await allSettled(
+    await Promise.all(
       uniqueIds.map(
         (id) =>
           new Promise(async (resolve, reject) => {
@@ -33,9 +38,12 @@ export class UpdateKeywordLikeCountConsumer {
                 LikeOwnerType.Keyword,
                 id
               );
-              resolve(this.keywordsRepository.update(id, { likeCount }));
-            } catch (err) {
-              reject({ id, reason: err });
+              const keyword = await this.keywordsRepository.update(id, {
+                likeCount,
+              });
+              resolve(keyword);
+            } catch (error) {
+              reject(`keywordId: ${id}, UpdateLikeCount Error: ${error}`);
             }
           })
       )
