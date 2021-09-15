5a97cebfb842b5fac24105a591572d1ec03e211f
Yang <ybh1760@gmail.com>
2021-09-15 14:30:39 +0900

    feat(job): :sparkles: add RemoveDeletedDigestImagesStep

---
+++ src/batch/jobs/content/remove-deleted-digest-images/steps/index.ts
@@ -0,0 +1,1 @@
+export * from './remove-deleted-digest-images.step';
---
+++ src/batch/jobs/content/remove-deleted-digest-images/steps/remove-deleted-digest-images.step.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { BaseStep } from '@batch/jobs/base.step';
+
+import { ImagesService } from '@mcommon/images/images.service';
+import { DigestImagesRepository } from '@content/digests/digests.repository';
+
+@Injectable()
+export class RemoveDeletedDigestImagesStep extends BaseStep {
+  constructor(
+    private readonly imagesService: ImagesService,
+    @InjectRepository(DigestImagesRepository)
+    private readonly digestImagesRepository: DigestImagesRepository
+  ) {
+    super();
+  }
+
+  async tasklet(): Promise<void> {
+    const deletedDigestImages = await this.digestImagesRepository.find({
+      digestId: null,
+    });
+    const deletedKeys = deletedDigestImages.map(({ key }) => key);
+    await this.digestImagesRepository.remove(deletedDigestImages);
+    await this.imagesService.removeByKeys(deletedKeys);
+  }
+}
