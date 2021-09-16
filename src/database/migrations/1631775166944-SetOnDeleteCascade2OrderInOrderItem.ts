578e2c38b979c817cabde85337f0691a69ac5b91
Yang <ybh1760@gmail.com>
2021-09-16 16:55:43 +0900

    enhance(infra): :art: remove unnecessary query string in SetOnDeleteCascade2OrderInOrderItem1631775166944

--- src/database/migrations/1631775166944-SetOnDeleteCascade2OrderInOrderItem.ts
+++ src/database/migrations/1631775166944-SetOnDeleteCascade2OrderInOrderItem.ts
@@ -7,19 +7,19 @@ export class SetOnDeleteCascade2OrderInOrderItem1631775166944
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`pickk_dev\`.\`order_item\` DROP FOREIGN KEY \`FK_cfa63773c75aa44395ebcd491ff\``
+      'ALTER TABLE `order_item` DROP FOREIGN KEY `FK_cfa63773c75aa44395ebcd491ff`'
     );
     await queryRunner.query(
-      `ALTER TABLE \`pickk_dev\`.\`order_item\` ADD CONSTRAINT \`FK_cfa63773c75aa44395ebcd491ff\` FOREIGN KEY (\`orderMerchantUid\`) REFERENCES \`pickk_dev\`.\`order\`(\`merchantUid\`) ON DELETE CASCADE ON UPDATE NO ACTION`
+      'ALTER TABLE `order_item` ADD CONSTRAINT `FK_cfa63773c75aa44395ebcd491ff` FOREIGN KEY (`orderMerchantUid`) REFERENCES `order`(`merchantUid`) ON DELETE CASCADE ON UPDATE NO ACTION'
     );
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `ALTER TABLE \`pickk_dev\`.\`order_item\` DROP FOREIGN KEY \`FK_cfa63773c75aa44395ebcd491ff\``
+      'ALTER TABLE `order_item` DROP FOREIGN KEY `FK_cfa63773c75aa44395ebcd491ff`'
     );
     await queryRunner.query(
-      `ALTER TABLE \`pickk_dev\`.\`order_item\` ADD CONSTRAINT \`FK_cfa63773c75aa44395ebcd491ff\` FOREIGN KEY (\`orderMerchantUid\`) REFERENCES \`pickk_dev\`.\`order\`(\`merchantUid\`) ON DELETE NO ACTION ON UPDATE NO ACTION`
+      'ALTER TABLE `order_item` ADD CONSTRAINT `FK_cfa63773c75aa44395ebcd491ff` FOREIGN KEY (`orderMerchantUid`) REFERENCES `order`(`merchantUid`) ON DELETE NO ACTION ON UPDATE NO ACTION'
     );
   }
 }
