ede65bbfad52a145d53b099bbd15b2a38043582b
Yang <ybh1760@gmail.com>
2021-09-17 18:17:22 +0900

    feat(provider): :sparkles: add sendLookCreation and sendVideoCreation to SlackService

--- src/providers/slack/provider.service.ts
+++ src/providers/slack/provider.service.ts
@@ -7,11 +7,15 @@ import {
   ItemCreationFailTemplate,
   ItemCreationSuccessTemplate,
   ItemReportTemplate,
+  LookCreationTemplate,
+  VideoCreationTemplate,
 } from '@templates/slack';
 
 import { Item } from '@item/items/models';
 import { Inquiry } from '@item/inquiries/models';
 import { Digest } from '@content/digests/models';
+import { Look } from '@content/looks/models';
+import { Video } from '@content/videos/models';
 
 @Injectable()
 export class SlackService {
@@ -53,5 +57,13 @@ export class SlackService {
 
   async sendDigestCreation(digest: Digest): Promise<IncomingWebhookResult> {
     return await this.webhook.send(DigestCreationTemplate.create(digest));
+  }
+
+  async sendLookCreation(look: Look): Promise<IncomingWebhookResult> {
+    return await this.webhook.send(LookCreationTemplate.create(look));
+  }
+
+  async sendVideoCreation(video: Video): Promise<IncomingWebhookResult> {
+    return await this.webhook.send(VideoCreationTemplate.create(video));
   }
 }
