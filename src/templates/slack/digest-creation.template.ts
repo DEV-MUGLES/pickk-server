d1e6cb91302fa41d72088ecb93d97093cd20af2d
Yang <ybh1760@gmail.com>
2021-09-17 17:43:50 +0900

    feat(templates): :sparkles: add DigestCreationTemplate

---
+++ src/templates/slack/digest-creation.template.ts
@@ -0,0 +1,36 @@
+import { IncomingWebhookSendArguments } from '@slack/webhook';
+import dayjs from 'dayjs';
+
+import { SlackChannelName } from '@providers/slack/constants';
+import { Digest } from '@content/digests/models';
+
+import { BaseSlackTemplate } from './base-slack.template';
+
+export class DigestCreationTemplate extends BaseSlackTemplate {
+  static create(digest: Digest): IncomingWebhookSendArguments {
+    const {
+      id,
+      user: { email, nickname },
+      title,
+      createdAt,
+    } = digest;
+    const pickkUrl = `https://pickk.one/digests/${id}`;
+
+    return {
+      channel: SlackChannelName.ContentUpload,
+      blocks: this.blocksBuilder
+        .addText(`:pushpin: *꿀템 등록 알림*`)
+        .addText(`*<${pickkUrl}|${title}>*`)
+        .addText(`*작성자*:\n${nickname}(<mailto:${email}|${email}>)`)
+        .addText(`*아이템*`)
+        .addButtons([{ text: '핔에서 보기', url: pickkUrl, style: 'primary' }])
+        .addContext(
+          `*<mailto:${email}|${email}(${nickname})>* at ${dayjs(createdAt)
+            .format('YYYY. M. D. a H:mm:ss')
+            .replace('pm', '오후')
+            .replace('am', '오전')}`
+        )
+        .build(),
+    };
+  }
+}
--- src/templates/slack/index.ts
+++ src/templates/slack/index.ts
@@ -2,3 +2,4 @@ export * from './item-creation-fail.template';
 export * from './item-creation-success.template';
 export * from './item-report.template';
 export * from './inquiry-creation.template';
+export * from './digest-creation.template';
