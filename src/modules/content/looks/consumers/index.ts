cc9a33255d94c0ae26e2e90d78601c2b1087eaf9
Yang <ybh1760@gmail.com>
2021-09-23 15:24:02 +0900

    feat(queue): :sparkles: add RemoveLookImagesConsumer

--- src/modules/content/looks/consumers/index.ts
+++ src/modules/content/looks/consumers/index.ts
@@ -1,9 +1,11 @@
 import { UpdateLookLikeCountConsumer } from './update-look-like-count.consumer';
 import { UpdateLookCommentCountConsumer } from './update-look-comment-count.consumer';
 import { SendLookCreationSlackMessageConsumer } from './send-look-creation-slack-message.consumer';
+import { RemoveLookImagesConsumer } from './remove-look-images.consumer';
 
 export const LooksConsumers = [
   UpdateLookLikeCountConsumer,
   UpdateLookCommentCountConsumer,
   SendLookCreationSlackMessageConsumer,
+  RemoveLookImagesConsumer,
 ];
