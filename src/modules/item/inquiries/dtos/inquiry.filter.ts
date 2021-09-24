5f25c0f050efcaccbfe10fe8e2c4fd25094664a4
greatSumini <greatSumini@gmail.com>
2021-09-24 20:38:28 +0900

    feat(application): :sparkles: add search,isAnswered to InquiryFilter

--- src/modules/item/inquiries/dtos/inquiry.filter.ts
+++ src/modules/item/inquiries/dtos/inquiry.filter.ts
@@ -1,9 +1,14 @@
 import { Field, InputType, Int } from '@nestjs/graphql';
 
 import { IInquiry } from '../interfaces';
+import { Inquiry } from '../models';
 
 @InputType()
 export class InquiryFilter implements Partial<IInquiry> {
+  searchFields?: Array<keyof Inquiry> = ['orderItemMerchantUid'];
+  @Field({ nullable: true, description: '주문상품번호로 검색 가능' })
+  search?: string;
+
   @Field(() => Int, { nullable: true })
   userId?: number;
 
@@ -12,4 +17,7 @@ export class InquiryFilter implements Partial<IInquiry> {
 
   @Field(() => Int, { nullable: true })
   sellerId?: number;
+
+  @Field({ nullable: true })
+  isAnswered?: boolean;
 }
