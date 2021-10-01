c57a416433ec165b0faa4c5ca0f3f4cf85ceb10e
Yang <ybh1760@gmail.com>
2021-10-01 19:47:09 +0900

    enhance(templates): :art: partialEncrypt order.buyer.name in alimtalk templates

--- src/templates/alimtalk/sellers/exchange-requested.template.ts
+++ src/templates/alimtalk/sellers/exchange-requested.template.ts
@@ -1,6 +1,6 @@
 import { AlimtalkMessageRequest } from 'nest-sens';
 
-import { format2Korean } from '@common/helpers';
+import { format2Korean, partialEncrypt } from '@common/helpers';
 import { getPurchaseItemInfo } from '@templates/helpers';
 
 import { ExchangeRequest } from '@order/exchange-requests/models';
@@ -38,7 +38,7 @@ export class ExchangeRequestedSellerTemplate {
       brand.nameKor
     } 담당자님, 교환요청이 접수되었습니다. 제품이 도착하여 검수단계를 거쳐 교환승인 처리 후 교환요청제품 발송 요청드립니다.
 ▶ 주문상품번호 : ${orderItem.merchantUid}
-▶ 구매자 : ${orderItem.order.buyer.name}
+▶ 구매자 : ${partialEncrypt(orderItem.order.buyer.name, 1)}
 ▶ 구매 상품 : ${getPurchaseItemInfo(orderItem)}
 ▶ 교환요청 상품 : ${itemName}(${productVariantName})
 ▶ 교환요청일시 : ${format2Korean(requestedAt)}
--- src/templates/alimtalk/sellers/refund-requested.template.ts
+++ src/templates/alimtalk/sellers/refund-requested.template.ts
@@ -1,6 +1,6 @@
 import { AlimtalkMessageRequest } from 'nest-sens';
 
-import { addCommas, format2Korean } from '@common/helpers';
+import { addCommas, format2Korean, partialEncrypt } from '@common/helpers';
 import { getPurchaseItemInfo, getTotalPayAmount } from '@templates/helpers';
 
 import { RefundRequest } from '@order/refund-requests/models';
@@ -37,7 +37,7 @@ export class RefundRequestedSellerTemplate {
       brand.nameKor
     } 담당자님, 환불요청이 접수되었습니다. 제품수령 후 환불승인 처리 요청드립니다.
 ▶ 주문번호 : ${order.merchantUid}
-▶ 구매자 : ${order.buyer.name}
+▶ 구매자 : ${partialEncrypt(order.buyer.name, 1)}
 ▶ 구매 상품 : ${getPurchaseItemInfo(orderItems)}
 ▶ 환불요청일시 : ${format2Korean(requestedAt)}
 ▶ 결제금액 : ${addCommas(getTotalPayAmount(orderItems))}원
