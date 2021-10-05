de8669c20def3f0552ef6dd7c0a74d8dda50f15c
Yang <ybh1760@gmail.com>
2021-10-05 17:23:43 +0900

    enhance(templates): :art: resolve InquiryCreatedTemplate changes

--- src/templates/alimtalk/sellers/inquiry-created.template.ts
+++ src/templates/alimtalk/sellers/inquiry-created.template.ts
@@ -5,61 +5,43 @@ import { getPurchaseItemInfo } from '@templates/helpers';
 import { Inquiry } from '@item/inquiries/models';
 
 export class InquiryCreatedTemplate {
+  static code = 'bqna03';
+
   static toRequest(
     inquiry: Inquiry
   ): Omit<AlimtalkMessageRequest, 'plusFriendId'> {
     const {
       seller: { phoneNumber },
     } = inquiry;
-    const TemplateClass = inquiry.orderItem ? Bqna01Template : Bqna02Template;
+
     return {
-      templateCode: TemplateClass.code,
+      templateCode: this.code,
       messages: [
         {
           to: phoneNumber,
-          content: TemplateClass.toContent(inquiry),
+          content: this.toContent(inquiry),
         },
       ],
     };
   }
-}
-
-class Bqna01Template {
-  static code = 'Bqna01';
 
   static toContent(inquiry: Inquiry): string {
     const {
       user,
       content,
       orderItem,
-      seller: { brand },
-    } = inquiry;
-    return `안녕하세요! ${
-      brand.nameKor
-    } 담당자님, 아래 제품에 대한 고객님의 문의가 접수되었습니다. 핔 어드민 서비스를 통해 문의에 답변을 남겨주세요.
-▶ 문의자 : ${user.name}
-▶ 문의상품 : ${getPurchaseItemInfo(orderItem)}
-▶ 문의내용 : ${content}
-핔 어드민 URL : https://admin.pickk.one/dashboard/
-    `;
-  }
-}
-
-class Bqna02Template {
-  static code = 'Bqna02';
-
-  static toContent(inquiry: Inquiry): string {
-    const {
-      user,
       item,
-      content,
       seller: { brand },
     } = inquiry;
-    return `안녕하세요! ${brand.nameKor} 담당자님, 아래 제품에 대한 고객님의 문의가 접수되었습니다. 문의에 답변을 남겨주세요.
-▶ 문의자 : ${user.name}
-▶ 문의상품 : ${item.name}
+    const itemInfo = inquiry.orderItem
+      ? getPurchaseItemInfo(orderItem)
+      : item.name;
+    const adminUrl = 'https://admin.pickk.one/inquiries/';
+    return `안녕하세요! ${brand.nameKor} 담당자님, 아래 제품에 대한 고객님의 문의가 접수되었습니다. 핔 어드민 서비스를 통해 문의에 답변을 남겨주세요.
+▶ 문의자 : ${user.nickname}
+▶ 문의상품 : ${itemInfo}
 ▶ 문의내용 : ${content}
-URL: https://pickk.one/item/${item.id}
-        `;
+핔 어드민 URL : ${adminUrl}
+    `;
   }
 }
