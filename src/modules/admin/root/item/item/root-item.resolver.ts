8906daa6b4496da562a178a7133cf54ae8dbb2ae
Yang <ybh1760@gmail.com>
2021-10-12 17:42:26 +0900

    feat(application): :sparkles: add RootItemModule, RootItemResolver

---
+++ src/modules/admin/root/item/item/root-item.resolver.ts
@@ -0,0 +1,32 @@
+import { UseGuards } from '@nestjs/common';
+import { Mutation, Resolver } from '@nestjs/graphql';
+
+import { Roles } from '@auth/decorators';
+import { JwtVerifyGuard } from '@auth/guards';
+import { IntArgs } from '@common/decorators';
+import { BaseResolver } from '@common/base.resolver';
+
+import { ItemRelationType, ITEM_RELATIONS } from '@item/items/constants';
+
+import { Item } from '@item/items/models';
+import { ItemsService } from '@item/items/items.service';
+import { UserRole } from '@user/users/constants';
+
+@Resolver(() => Item)
+export class RootItemResolver extends BaseResolver<ItemRelationType> {
+  relations = ITEM_RELATIONS;
+
+  constructor(private readonly itemsService: ItemsService) {
+    super();
+  }
+
+  @UseGuards(JwtVerifyGuard)
+  @Roles(UserRole.Admin)
+  @Mutation(() => Item)
+  async updateRootItemImageUrl(
+    @IntArgs('itemId') itemId: number
+  ): Promise<Item> {
+    await this.itemsService.updateImageUrl(itemId);
+    return await this.itemsService.get(itemId);
+  }
+}
