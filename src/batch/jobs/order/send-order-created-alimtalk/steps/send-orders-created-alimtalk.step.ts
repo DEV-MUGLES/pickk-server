4137a8afb90b5487736c0d32d67f1d589ca84696
양병훈 <ybh1760@gmail.com>
2021-10-13 19:24:21 +0900

    enhance(job): :art: add time, claimStatus constraints to OrderCounts … (#299)
    * enhance(job): :art: add time, claimStatus constraints to OrderCounts query
    
    * enhance(job): :art: change getTimeRanges return type

--- src/batch/jobs/order/send-order-created-alimtalk/steps/send-orders-created-alimtalk.step.ts
+++ src/batch/jobs/order/send-order-created-alimtalk/steps/send-orders-created-alimtalk.step.ts
@@ -1,10 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import dayjs from 'dayjs';
+import timezone from 'dayjs/plugin/timezone';
+import utc from 'dayjs/plugin/utc';
 import { getConnection } from 'typeorm';
 
 import { BaseStep } from '@batch/jobs/base.step';
-
+import { Timezone } from '@common/constants';
 import { AlimtalkService } from '@providers/sens';
+
 import { OrderItemStatus } from '@order/order-items/constants';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
 
 @Injectable()
 export class SendOrdersCreatedAlimtalkStep extends BaseStep {
@@ -13,22 +20,32 @@ export class SendOrdersCreatedAlimtalkStep extends BaseStep {
   }
 
   async tasklet() {
+    const [start, end] = this.getTimeRanges();
     const runner = getConnection().createQueryRunner();
     const ordersCountBySeller = await runner.query(`
       SELECT sum(orderCountData.count) as ordersCount, orderCountData.brandKor, orderCountData.phoneNumber
       FROM(
         SELECT count(oi.orderMerchantUid) as count, b.nameKor as brandKor, s.orderNotiPhoneNumber as phoneNumber, oi.sellerId
-        FROM order_item as oi        
+        FROM order_item as oi
         JOIN seller as s
           ON s.id=oi.sellerId
         JOIN brand as b
           ON b.id=s.brandId
         WHERE oi.status='${OrderItemStatus.Paid}'
+        AND oi.claimStatus is null
+        AND oi.createdAt BETWEEN '${start.format(
+          'YYYY-MM-DD hh:mm:ss'
+        )}' AND '${end.format('YYYY-MM-DD hh:mm:ss')}'
         GROUP BY oi.orderMerchantUid, oi.sellerId
       ) as orderCountData
       GROUP BY orderCountData.sellerId;
     `);
     runner.release();
+
+    if (ordersCountBySeller.length === 0) {
+      return;
+    }
+
     for (const { brandKor, phoneNumber, ordersCount } of ordersCountBySeller) {
       await this.alimtalkService.sendOrdersCreated(
         {
@@ -38,5 +55,23 @@ export class SendOrdersCreatedAlimtalkStep extends BaseStep {
         ordersCount
       );
     }
+  }
+
+  private getTimeRanges(): [dayjs.Dayjs, dayjs.Dayjs] {
+    const current = dayjs();
+    const hourDiff = this.is10oclock() ? 19 : 5;
+
+    if (this.isMonday() && this.is10oclock()) {
+      return [current.subtract(2, 'day').subtract(hourDiff, 'hour'), current];
+    }
+    return [current.subtract(hourDiff, 'hour'), current];
+  }
+
+  private isMonday(): boolean {
+    return dayjs().tz(Timezone.Seoul).get('day') === 1;
+  }
+
+  private is10oclock(): boolean {
+    return dayjs().tz(Timezone.Seoul).get('hour') === 10;
   }
 }
