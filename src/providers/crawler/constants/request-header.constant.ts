92ef2f1314a6a27a4304504ed2025412be30cd78
양병훈 <ybh1760@gmail.com>
2021-10-14 21:09:07 +0900

    enhance(provider): :art: add options(timeout, headers) to http reques… (#314)
    * enhance(provider): :art: add options(timeout, headers) to http request in checkUrlExisting
    
    * chore: :fire: remove console.log

---
+++ src/providers/crawler/constants/request-header.constant.ts
@@ -0,0 +1,12 @@
+export const requestHeaderObjects = {
+  'maveworks.com': {
+    accept:
+      'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'ko,en-US;q=0.9,en;q=0.8,ko-KR;q=0.7',
+    cookie:
+      '2a0d2363701f23f8a75028924a3af643=MjE4LjQ5LjIyNi4zMQ%3D%3D; PHPSESSID=juj55mv2r7altbd464njruv106; _ga=GA1.2.494630211.1613464802; _gid=GA1.2.96733555.1613464802; _fbp=fb.1.1613464803155.1078587594; 5b1ceb69146c0bafdc082ff42248da98=MTU5ODgzNjc1MQ%3D%3D; _gat_gtag_UA_187577754_2=1; wcs_bt=s_1ffa960b15e1:1613469220',
+    'user-agent':
+      'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36',
+  },
+};
