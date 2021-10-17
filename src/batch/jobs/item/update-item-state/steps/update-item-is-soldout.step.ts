9e22e29c5243b98cbf4ea856c4908a6578c34d84
greatSumini <greatSumini@gmail.com>
2021-10-18 03:37:46 +0900

    enhance(job): update UpdateItemIsSoldoutStep to process purchasable items only

--- src/batch/jobs/item/update-item-state/steps/update-item-is-soldout.step.ts
+++ src/batch/jobs/item/update-item-state/steps/update-item-is-soldout.step.ts
@@ -22,40 +22,44 @@ export class UpdateItemIsSoldoutStep extends BaseStep {
       itemId: number;
       totalStock: number;
       isInfiniteStock: boolean;
+      isSoldout: boolean;
     }> = (
       await this.productsRepository
         .createQueryBuilder('product')
         .leftJoin('product.shippingReservePolicy', 'reservedProduct')
         .leftJoin('product.item', 'item')
         .select(
-          'sum(product.stock) as stocks, product.itemId, sum(reservedProduct.stock) as reservedStocks, item.isInfiniteStock'
+          'sum(product.stock) as stocks, product.itemId, sum(reservedProduct.stock) as reservedStocks, item.isInfiniteStock, item.isSoldout'
         )
+        .where('item.isPurchasable = true')
         .addGroupBy('itemId')
         .execute()
-    ).map(({ itemId, isInfiniteStock, stocks, reservedStocks }) => ({
+    ).map(({ itemId, isInfiniteStock, stocks, reservedStocks, isSoldout }) => ({
       itemId: Number(itemId),
       totalStock: Number(stocks) + Number(reservedStocks ?? 0),
       isInfiniteStock,
+      isSoldout,
     }));
 
-    const isSolout = (totalStock: number, isInfiniteStock: boolean) => {
+    const checkSoldout = (totalStock: number, isInfiniteStock: boolean) => {
       if (isInfiniteStock) return false;
       if (totalStock > 0) return false;
       return true;
     };
 
     const soldOutItemIds = productStockDatas
-      .filter(({ totalStock, isInfiniteStock }) =>
-        isSolout(totalStock, isInfiniteStock)
+      .filter(
+        ({ totalStock, isInfiniteStock, isSoldout }) =>
+          checkSoldout(totalStock, isInfiniteStock) && !isSoldout
       )
-      .map(({ itemId }) => itemId);
+      .map((v) => v.itemId);
 
     const notSoldOutItemIds = productStockDatas
       .filter(
-        ({ totalStock, isInfiniteStock }) =>
-          !isSolout(totalStock, isInfiniteStock)
+        ({ totalStock, isInfiniteStock, isSoldout }) =>
+          !checkSoldout(totalStock, isInfiniteStock) && isSoldout
       )
-      .map(({ itemId }) => itemId);
+      .map((v) => v.itemId);
 
     await this.itemsRepository.bulkUpdate(soldOutItemIds, { isSoldout: true });
     await this.itemsRepository.bulkUpdate(notSoldOutItemIds, {
