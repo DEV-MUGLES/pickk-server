eb4f00768673affb3a10c5e936bc5f8e4db6c778
= <ybh1760@gmail.com>
2021-10-21 09:58:08 +0900

    enhance(application): :art: change ItemSizeChart interface

--- src/modules/item/items/interfaces/item-size-chart.interface.ts
+++ src/modules/item/items/interfaces/item-size-chart.interface.ts
@@ -1,38 +1,18 @@
 import { IBaseId } from '@common/interfaces';
 
-import { ItemSizeChartColumnName } from '../constants';
-
-import { IItem } from './item.interface';
-
 export interface IItemSizeChart extends IBaseId {
-  name: string;
-  //상의,아우터,하의,악세사리등 총 길이와 관련된 정보
-  totalLength?: number;
-  //상의,아우터의 사이즈 정보
-  shoulderWidth?: number;
-  chestWidth?: number;
-  sleeveLength?: number;
-  waistWidth?: number;
-  //하의의 사이즈 정보
-  riseHeight?: number;
-  thighWidth?: number;
-  hemWidth?: number;
-  //악세사리(가방,시계,지갑,벨트,안경렌즈)의 사이즈 정보
-  accWidth?: number;
-  accHeight?: number;
-  accDepth?: number;
-  //가방,시계,안경의 사이즈 정보
-  crossStrapLength?: number;
-  watchBandDepth?: number;
-  glassWidth?: number;
-  glassBridgeLength?: number;
-  glassLegLength?: number;
-
-  item?: IItem;
-  itemId: number;
+  labels: string[];
+  sizes: IItemSize[];
+  recommendations: IItemSizeRecommendation[];
 }
 
-export interface IItemSizeChartMetaData {
-  columnName: ItemSizeChartColumnName;
-  displayName: string;
+export interface IItemSize {
+  name: string;
+  values: string[];
+}
+
+export interface IItemSizeRecommendation {
+  height: number;
+  weight: number;
+  sizeName: string;
 }
