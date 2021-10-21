d7054967842347347bc82e83aa7ad2d90fc0c75e
= <ybh1760@gmail.com>
2021-10-21 10:47:48 +0900

    fix(application): :art: fix CreateItemSizeChartInput sizes, recommendations field error

--- src/modules/item/items/dtos/item-size-chart.input.ts
+++ src/modules/item/items/dtos/item-size-chart.input.ts
@@ -1,13 +1,28 @@
-import { InputType, PickType } from '@nestjs/graphql';
+import { Field, InputType, PickType } from '@nestjs/graphql';
 
-import { ItemSizeChart } from '../models';
+import { ItemSize, ItemSizeChart, ItemSizeRecommendation } from '../models';
+
+@InputType()
+class ItemSizeInput extends PickType(ItemSize, ['name', 'values'], InputType) {}
+
+@InputType()
+class ItemSizeRecommendationInput extends PickType(
+  ItemSizeRecommendation,
+  ['height', 'sizeName', 'weight'],
+  InputType
+) {}
 
 @InputType()
 export class CreateItemSizeChartInput extends PickType(
   ItemSizeChart,
-  ['labels', 'recommendations', 'sizes'],
+  ['labels'],
   InputType
-) {}
+) {
+  @Field(() => [ItemSizeInput])
+  sizes: ItemSizeInput[];
+  @Field(() => [ItemSizeRecommendationInput], { nullable: true })
+  recommendations: ItemSizeRecommendationInput[];
+}
 
 @InputType()
 export class UpdateItemSizeChartInput extends CreateItemSizeChartInput {}
