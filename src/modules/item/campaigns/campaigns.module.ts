dd2b10015955267fed901ae7682eb8976d5207e3
= <ybh1760@gmail.com>
2021-10-29 10:59:48 +0900

    enhance: :truck: rename capaigns.respository to campaigns.respository

--- src/modules/item/campaigns/campaigns.module.ts
+++ src/modules/item/campaigns/campaigns.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
-import { CampaignsRepository } from './capaigns.repository';
+import { CampaignsRepository } from './campaigns.repository';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CampaignsRepository])],
