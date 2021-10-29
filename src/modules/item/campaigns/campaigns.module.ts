57684a6c8cc0e96fcf36edaa6caf521142d4740a
= <ybh1760@gmail.com>
2021-10-29 13:38:31 +0900

    feat(application): :sparkles: add CampaignsResolver

--- src/modules/item/campaigns/campaigns.module.ts
+++ src/modules/item/campaigns/campaigns.module.ts
@@ -2,8 +2,11 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { CampaignsRepository } from './campaigns.repository';
+import { CampaignsResolver } from './campaigns.resolver';
+import { CampaignsService } from './campaigns.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([CampaignsRepository])],
+  providers: [CampaignsResolver, CampaignsService],
 })
 export class CampaignsModule {}
