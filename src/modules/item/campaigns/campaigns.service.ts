391baee92c28497b83d40a8edeccf06bbf9b44ef
= <ybh1760@gmail.com>
2021-10-29 13:38:10 +0900

    feat(application): :sparkles: add CampaignsService

---
+++ src/modules/item/campaigns/campaigns.service.ts
@@ -0,0 +1,53 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+
+import { CampaignRelationType } from './constants';
+import { Campaign } from './models';
+
+import { CampaignsRepository } from './campaigns.repository';
+import { CreateCampaignInput, UpdateCampaignInput } from './dtos';
+
+@Injectable()
+export class CampaignsService {
+  constructor(
+    @InjectRepository(CampaignsRepository)
+    private readonly campaignsRepository: CampaignsRepository
+  ) {}
+
+  async get(
+    id: number,
+    relations: CampaignRelationType[] = []
+  ): Promise<Campaign> {
+    return await this.campaignsRepository.get(id, relations);
+  }
+
+  async list(relations: CampaignRelationType[] = []): Promise<Campaign[]> {
+    return this.campaignsRepository.entityToModelMany(
+      await this.campaignsRepository.find({ relations })
+    );
+  }
+
+  async create(input: CreateCampaignInput): Promise<Campaign> {
+    return this.campaignsRepository.entityToModel(
+      await this.campaignsRepository.save(new Campaign(input))
+    );
+  }
+
+  async update(id: number, input: UpdateCampaignInput): Promise<Campaign> {
+    const campaign = await this.get(id);
+    if (!campaign) {
+      throw new BadRequestException('존재하지 않는 캠페인입니다.');
+    }
+
+    return this.campaignsRepository.entityToModel(
+      await this.campaignsRepository.save(
+        new Campaign({ ...campaign, ...input })
+      )
+    );
+  }
+
+  async remove(id: number) {
+    const campaign = await this.get(id);
+    await this.campaignsRepository.remove(campaign);
+  }
+}
