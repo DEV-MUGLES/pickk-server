763d044fe35c1c98ded323b0a7c9c92c0aa08302
= <ybh1760@gmail.com>
2021-10-29 14:02:19 +0900

    enhance(application): :fire: remove mutations in CampaignsResolver

--- src/modules/item/campaigns/campaigns.resolver.ts
+++ src/modules/item/campaigns/campaigns.resolver.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
-import { Args, Info, Int, Mutation, Query } from '@nestjs/graphql';
+import { Info, Query } from '@nestjs/graphql';
 import { GraphQLResolveInfo } from 'graphql';
 
 import { BaseResolver } from '@common/base.resolver';
 import { IntArgs } from '@common/decorators';
 
 import { CampaignRelationType, CAMPAIGN_RELATIONS } from './constants';
-import { CreateCampaignInput, UpdateCampaignInput } from './dtos';
 import { Campaign } from './models';
 
 import { CampaignsService } from './campaigns.service';
@@ -30,32 +29,4 @@ export class CampaignsResolver extends BaseResolver<CampaignRelationType> {
   async campaigns(@Info() info?: GraphQLResolveInfo): Promise<Campaign[]> {
     return await this.campaignsService.list(this.getRelationsFromInfo(info));
   }
-
-  @Mutation(() => Campaign)
-  async createCampaign(
-    @Args('input') input: CreateCampaignInput
-  ): Promise<Campaign> {
-    return await this.campaignsService.create(input);
-  }
-
-  @Mutation(() => Campaign)
-  async updateCampaign(
-    @IntArgs('id') id: number,
-    @Args('input') input: UpdateCampaignInput
-  ) {
-    await this.campaignsService.update(id, input);
-    return await this.campaignsService.get(id);
-  }
-
-  @Mutation(() => Boolean)
-  async removeCampaign(@IntArgs('id') id: number): Promise<Boolean> {
-    await this.campaignsService.remove(id);
-    return true;
-  }
-
-  @Mutation(() => Campaign)
-  async updateCampaignItems() {}
-
-  @Mutation(() => Campaign)
-  async removeCampaignItems() {}
 }
