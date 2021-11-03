f169fdb5862f96a546db971763942ffb8842ea5b
= <ybh1760@gmail.com>
2021-11-03 17:00:50 +0900

    enhance: enhance ExchangeRequestRejectedStrategy exception message

--- src/modules/order/exchange-requests/strategies/rejected.strategy.ts
+++ src/modules/order/exchange-requests/strategies/rejected.strategy.ts
@@ -14,7 +14,7 @@ export class ExchangeRequestRejectedStrategy extends ExchangeRequestMarkStrategy
       this.exchangeRequest.status !== ExchangeRequestStatus.Requested
     ) {
       throw new BadRequestException(
-        `교환요청(${this.exchangeRequest.merchantUid})가 요청됨 상태가 아닙니다.`
+        `교환요청(${this.exchangeRequest.merchantUid})가 요청됨 또는 수거완료 상태가 아닙니다.`
       );
     }
   }
