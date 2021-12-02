060bf4852e68867421fea4bf59a08438d1cd2b08
greatSumini <greatSumini@gmail.com>
2021-12-03 02:40:32 +0900

    fix(job): :bug: fix TrackShipmentsStep

--- src/batch/jobs/shipment/track-shipments/steps/track-shipments.step.ts
+++ src/batch/jobs/shipment/track-shipments/steps/track-shipments.step.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Not } from 'typeorm';
+import { IsNull, Not } from 'typeorm';
 import { plainToClass } from 'class-transformer';
 import dayjs from 'dayjs';
 
 import { BaseStep } from '@batch/jobs/base.step';
 import { JobExecutionContext } from '@batch/models';
-import { allSettled } from '@common/helpers';
 import { DeliveryTrackerService } from '@providers/delivery-tracker';
 
 import { ExchangeRequestsService } from '@order/exchange-requests/exchange-requests.service';
@@ -42,62 +41,46 @@ export class TrackShipmentsStep extends BaseStep {
   async tasklet(context: JobExecutionContext): Promise<void> {
     const shipments = this.shipmentsRepository.entityToModelMany(
       await this.shipmentsRepository.find({
-        where: { status: ShipmentStatus.Shipping, ownerPk: Not('null') },
+        where: { status: ShipmentStatus.Shipping, ownerPk: Not(IsNull()) },
         relations: ['courier', 'histories'],
       })
     );
 
-    const historiesMap = new Map<number, ShipmentHistory[]>();
-    const CHUNK_SIZE = 50;
-    for (let i = 0; i < shipments.length; i += CHUNK_SIZE) {
-      await allSettled(
-        shipments.slice(i, i + CHUNK_SIZE).map(
-          (v) =>
-            new Promise<void>(async (resolve, reject) => {
-              try {
-                const histories = await this.getRecentHistories(v);
-                historiesMap.set(v.id, histories);
-                resolve();
-              } catch (error) {
-                context.put(
-                  v.id.toString(),
-                  error?.config?.url ?? error.message
-                );
-                reject();
-              }
-            })
-        )
-      );
-    }
-
     for (const shipment of shipments) {
-      const recentHistories = historiesMap.get(shipment.id);
-      if (!recentHistories) {
-        continue;
+      try {
+        const recentHistories = await this.getRecentHistories(shipment);
+        if (!recentHistories) {
+          continue;
+        }
+
+        const duplicatedHistories = this.findDuplicatedHistories(
+          shipment.histories
+        );
+        const deletedHistories = this.findDeletedHistories(
+          shipment.histories,
+          recentHistories
+        );
+
+        delete shipment.histories;
+        await this.shipmentsHistoriesRepository.save(recentHistories);
+        await this.shipmentsHistoriesRepository.remove([
+          ...deletedHistories,
+          ...duplicatedHistories,
+        ]);
+
+        if (this.isShippedShipment(recentHistories)) {
+          shipment.status = ShipmentStatus.Shipped;
+          await this.processShippedShipment(shipment);
+        }
+
+        shipment.lastTrackedAt = new Date();
+        await this.shipmentsRepository.save(shipment);
+      } catch (err) {
+        context.put(
+          shipment.id.toString(),
+          err.message + '/' + err?.config?.url
+        );
       }
-
-      const duplicatedHistories = this.findDuplicatedHistories(
-        shipment.histories
-      );
-      const deletedHistories = this.findDeletedHistories(
-        shipment.histories,
-        recentHistories
-      );
-
-      delete shipment.histories;
-      await this.shipmentsHistoriesRepository.save(recentHistories);
-      await this.shipmentsHistoriesRepository.remove([
-        ...deletedHistories,
-        ...duplicatedHistories,
-      ]);
-
-      if (this.isShippedShipment(recentHistories)) {
-        shipment.status = ShipmentStatus.Shipped;
-        await this.processShippedShipment(shipment);
-      }
-
-      shipment.lastTrackedAt = new Date();
-      await this.shipmentsRepository.save(shipment);
     }
   }
 
